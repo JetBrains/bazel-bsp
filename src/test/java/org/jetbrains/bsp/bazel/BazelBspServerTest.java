6f97eb25bf90f71e70849759707ae984f6ba3756
Abrams27 <abramowiczmarcin99@gmail.com>
2020-11-30 19:05:45 +0100

    code reformat

--- src/test/java/org/jetbrains/bsp/bazel/BazelBspServerTest.java
+++ src/test/java/org/jetbrains/bsp/bazel/BazelBspServerTest.java
@@ -8,9 +8,8 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
-
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class BazelBspServerTest {
 
@@ -22,9 +21,8 @@ public class BazelBspServerTest {
   public BazelBspServerTest() {
     LOGGER.info("Creating TestClient...");
 
-    this.client = TestClient$
-        .MODULE$
-        .testInitialStructure(
+    this.client =
+        TestClient$.MODULE$.testInitialStructure(
             BazelBspServerTestData.WORKSPACE_FULL_PATH,
             ImmutableMap.of(),
             BazelBspServerTestData.TEST_CLIENT_TIMEOUT_IN_MINUTES);
@@ -41,35 +39,47 @@ public class BazelBspServerTest {
 
   private List<BazelBspServerSingleTest> getTestsTopRun() {
     return ImmutableList.of(
-        new BazelBspServerSingleTest(
-            "resolve project",
-            client::testResolveProject),
+        new BazelBspServerSingleTest("resolve project", client::testResolveProject),
         new BazelBspServerSingleTest(
             "compare workspace targets results",
-            () -> client.testCompareWorkspaceTargetsResults(BazelBspServerTestData.EXPECTED_BUILD_TARGETS)),
+            () ->
+                client.testCompareWorkspaceTargetsResults(
+                    BazelBspServerTestData.EXPECTED_BUILD_TARGETS)),
         new BazelBspServerSingleTest(
             "sources results",
-            () -> client.testSourcesResults(BazelBspServerTestData.EXPECTED_BUILD_TARGETS, BazelBspServerTestData.EXPECTED_SOURCES)),
+            () ->
+                client.testSourcesResults(
+                    BazelBspServerTestData.EXPECTED_BUILD_TARGETS,
+                    BazelBspServerTestData.EXPECTED_SOURCES)),
         new BazelBspServerSingleTest(
             "resources results",
-            () -> client.testResourcesResults(BazelBspServerTestData.EXPECTED_BUILD_TARGETS, BazelBspServerTestData.EXPECTED_RESOURCES)),
+            () ->
+                client.testResourcesResults(
+                    BazelBspServerTestData.EXPECTED_BUILD_TARGETS,
+                    BazelBspServerTestData.EXPECTED_RESOURCES)),
         new BazelBspServerSingleTest(
             "inverse sources results",
-            () -> client.testInverseSourcesResults(BazelBspServerTestData.INVERSE_SOURCES_DOCUMENT, BazelBspServerTestData.EXPECTED_INVERSE_SOURCES)),
+            () ->
+                client.testInverseSourcesResults(
+                    BazelBspServerTestData.INVERSE_SOURCES_DOCUMENT,
+                    BazelBspServerTestData.EXPECTED_INVERSE_SOURCES)),
         new BazelBspServerSingleTest(
             "dependency sources results",
-            () -> client.testDependencySourcesResults(BazelBspServerTestData.EXPECTED_BUILD_TARGETS, BazelBspServerTestData.EXPECTED_DEPENDENCIES))
-//         TODO one day we will uncomment them...
-//        new BazelBspServerSingleTest(
-//            "targets run unsuccessfully",
-//            client::testTargetsRunUnsuccessfully),
-//        new BazelBspServerSingleTest(
-//            "targets test unsuccessfully",
-//            client::testTargetsTestUnsuccessfully),
-//            new BazelBspServerSingleTest(
-//                "target capabilities",
-//                client::testTargetCapabilities)
-    );
+            () ->
+                client.testDependencySourcesResults(
+                    BazelBspServerTestData.EXPECTED_BUILD_TARGETS,
+                    BazelBspServerTestData.EXPECTED_DEPENDENCIES))
+        //         TODO one day we will uncomment them...
+        //        new BazelBspServerSingleTest(
+        //            "targets run unsuccessfully",
+        //            client::testTargetsRunUnsuccessfully),
+        //        new BazelBspServerSingleTest(
+        //            "targets test unsuccessfully",
+        //            client::testTargetsTestUnsuccessfully),
+        //            new BazelBspServerSingleTest(
+        //                "target capabilities",
+        //                client::testTargetCapabilities)
+        );
   }
 
   private void runTests(List<BazelBspServerSingleTest> testsToRun) {
@@ -79,7 +89,8 @@ public class BazelBspServerTest {
     exitProgramWithSuccessIfAllTestPassed(didAllTestsPass);
   }
 
-  private List<BazelBspServerSingleTest> submitTestsForExecution(List<BazelBspServerSingleTest> testsToSubmit) {
+  private List<BazelBspServerSingleTest> submitTestsForExecution(
+      List<BazelBspServerSingleTest> testsToSubmit) {
     LOGGER.info("Submitting tests for execution...");
 
     return testsToSubmit.stream()
@@ -87,12 +98,15 @@ public class BazelBspServerTest {
         .collect(Collectors.toList());
   }
 
-  private boolean executeAllTestsAndReturnTrueIfAllPassed(List<BazelBspServerSingleTest> submittedTests) {
+  private boolean executeAllTestsAndReturnTrueIfAllPassed(
+      List<BazelBspServerSingleTest> submittedTests) {
     LOGGER.info("Executing tests...");
 
     return submittedTests.stream()
-        .allMatch(test ->
-            test.executeTestWithTimeoutAndReturnTrueIfPassed(BazelBspServerTestData.TEST_EXECUTION_TIMEOUT_IN_MINUTES));
+        .allMatch(
+            test ->
+                test.executeTestWithTimeoutAndReturnTrueIfPassed(
+                    BazelBspServerTestData.TEST_EXECUTION_TIMEOUT_IN_MINUTES));
   }
 
   private void exitProgramWithSuccessIfAllTestPassed(boolean didAllTestsPass) {
--- src/test/java/org/jetbrains/bsp/bazel/BazelBspServerTestData.java
+++ src/test/java/org/jetbrains/bsp/bazel/BazelBspServerTestData.java
@@ -20,27 +20,66 @@ import java.util.List;
 
 class BazelBspServerTestData {
 
+  static final Duration TEST_CLIENT_TIMEOUT_IN_MINUTES = Duration.ofMinutes(4);
+  static final Integer TEST_EXECUTION_TIMEOUT_IN_MINUTES = 15;
+  static final List<String> DEPENDENCIES =
+      ImmutableList.of(
+          "https/repo1.maven.org/maven2/com/google/j2objc/j2objc-annotations/1.3/j2objc-annotations-1.3-sources.jar",
+          "https/repo1.maven.org/maven2/com/google/code/findbugs/jsr305/3.0.2/jsr305-3.0.2-sources.jar",
+          "https/repo1.maven.org/maven2/com/google/guava/failureaccess/1.0.1/failureaccess-1.0.1-sources.jar",
+          "https/repo1.maven.org/maven2/com/google/errorprone/error_prone_annotations/2.3.2/error_prone_annotations-2.3.2-sources.jar",
+          "https/repo1.maven.org/maven2/org/codehaus/mojo/animal-sniffer-annotations/1.17/animal-sniffer-annotations-1.17-sources.jar",
+          "https/repo1.maven.org/maven2/org/checkerframework/checker-qual/2.8.1/checker-qual-2.8.1-sources.jar",
+          "https/repo1.maven.org/maven2/com/google/guava/guava/28.0-jre/guava-28.0-jre-sources.jar");
+  static final TextDocumentIdentifier INVERSE_SOURCES_DOCUMENT =
+      new TextDocumentIdentifier("file://" + WORKSPACE_FULL_PATH + "/dep/Dep.scala");
   private static final String JAVA = "java";
   private static final String SCALA = "scala";
-
   private static final String SAMPLE_REPO_PATH = "sample-repo";
   private static final String SAMPLE_REPO_EXAMPLE_PATH = SAMPLE_REPO_PATH + "/example";
   private static final String SAMPLE_REPO_DEP_PATH = SAMPLE_REPO_PATH + "/dep";
-
   private static final String BUILD_WORKSPACE_DIRECTORY = "BUILD_WORKSPACE_DIRECTORY";
   private static final String WORKSPACE_DIR_PATH = System.getenv(BUILD_WORKSPACE_DIRECTORY);
-
-  private static final BuildTargetIdentifier ID_1 = new BuildTargetIdentifier("//example:example");
-  private static final BuildTargetIdentifier ID_2 = new BuildTargetIdentifier("//dep:dep");
-  private static final BuildTargetIdentifier ID_3 = new BuildTargetIdentifier("//dep/deeper:deeper");
-
-
-  static final Duration TEST_CLIENT_TIMEOUT_IN_MINUTES = Duration.ofMinutes(4);
-  static final Integer TEST_EXECUTION_TIMEOUT_IN_MINUTES = 15;
-
   static final String WORKSPACE_FULL_PATH = WORKSPACE_DIR_PATH + "/" + SAMPLE_REPO_PATH;
+  private static final BuildTargetIdentifier ID_1 = new BuildTargetIdentifier("//example:example");
+  static final ResourcesResult EXPECTED_RESOURCES =
+      new ResourcesResult(
+          ImmutableList.of(
+              new ResourcesItem(
+                  ID_1,
+                  ImmutableList.of(
+                      SAMPLE_REPO_EXAMPLE_PATH + "/file.txt",
+                      SAMPLE_REPO_EXAMPLE_PATH + "/file2.txt"))));
+  private static final BuildTargetIdentifier ID_2 = new BuildTargetIdentifier("//dep:dep");
+  static final DependencySourcesResult EXPECTED_DEPENDENCIES =
+      new DependencySourcesResult(
+          ImmutableList.of(
+              new DependencySourcesItem(ID_1, DEPENDENCIES),
+              new DependencySourcesItem(ID_2, DEPENDENCIES)));
 
-
+  static final SourcesResult EXPECTED_SOURCES =
+      new SourcesResult(
+          ImmutableList.of(
+              new SourcesItem(
+                  ID_1,
+                  ImmutableList.of(
+                      new SourceItem(
+                          SAMPLE_REPO_EXAMPLE_PATH + "/Example.scala",
+                          SourceItemKind.FILE,
+                          false))),
+              new SourcesItem(
+                  ID_2,
+                  ImmutableList.of(
+                      new SourceItem(
+                          SAMPLE_REPO_DEP_PATH + "/Test.scala", SourceItemKind.FILE, false),
+                      new SourceItem(
+                          SAMPLE_REPO_DEP_PATH + "/JavaTest.java", SourceItemKind.FILE, false),
+                      new SourceItem(
+                          SAMPLE_REPO_DEP_PATH + "/Dep.scala", SourceItemKind.FILE, false)))));
+  static final InverseSourcesResult EXPECTED_INVERSE_SOURCES =
+      new InverseSourcesResult(ImmutableList.of(ID_2));
+  private static final BuildTargetIdentifier ID_3 =
+      new BuildTargetIdentifier("//dep/deeper:deeper");
   static final WorkspaceBuildTargetsResult EXPECTED_BUILD_TARGETS =
       new WorkspaceBuildTargetsResult(
           ImmutableList.of(
@@ -56,52 +95,4 @@ class BazelBspServerTestData {
                   ImmutableList.of(JAVA, SCALA),
                   ImmutableList.of(ID_3),
                   new BuildTargetCapabilities(true, false, false))));
-
-
-  static final List<String> DEPENDENCIES =
-      ImmutableList.of(
-          "https/repo1.maven.org/maven2/com/google/j2objc/j2objc-annotations/1.3/j2objc-annotations-1.3-sources.jar",
-          "https/repo1.maven.org/maven2/com/google/code/findbugs/jsr305/3.0.2/jsr305-3.0.2-sources.jar",
-          "https/repo1.maven.org/maven2/com/google/guava/failureaccess/1.0.1/failureaccess-1.0.1-sources.jar",
-          "https/repo1.maven.org/maven2/com/google/errorprone/error_prone_annotations/2.3.2/error_prone_annotations-2.3.2-sources.jar",
-          "https/repo1.maven.org/maven2/org/codehaus/mojo/animal-sniffer-annotations/1.17/animal-sniffer-annotations-1.17-sources.jar",
-          "https/repo1.maven.org/maven2/org/checkerframework/checker-qual/2.8.1/checker-qual-2.8.1-sources.jar",
-          "https/repo1.maven.org/maven2/com/google/guava/guava/28.0-jre/guava-28.0-jre-sources.jar");
-
-  static final DependencySourcesResult EXPECTED_DEPENDENCIES =
-      new DependencySourcesResult(
-          ImmutableList.of(
-              new DependencySourcesItem(ID_1, DEPENDENCIES),
-              new DependencySourcesItem(ID_2, DEPENDENCIES)));
-
-
-  static final SourcesResult EXPECTED_SOURCES =
-      new SourcesResult(
-          ImmutableList.of(
-              new SourcesItem(
-                  ID_1,
-                  ImmutableList.of(
-                      new SourceItem(SAMPLE_REPO_EXAMPLE_PATH + "/Example.scala", SourceItemKind.FILE, false))),
-              new SourcesItem(
-                  ID_2,
-                  ImmutableList.of(
-                      new SourceItem(SAMPLE_REPO_DEP_PATH + "/Test.scala", SourceItemKind.FILE, false),
-                      new SourceItem(SAMPLE_REPO_DEP_PATH + "/JavaTest.java", SourceItemKind.FILE, false),
-                      new SourceItem(SAMPLE_REPO_DEP_PATH + "/Dep.scala", SourceItemKind.FILE, false)))));
-
-
-  static final ResourcesResult EXPECTED_RESOURCES =
-      new ResourcesResult(
-          ImmutableList.of(
-              new ResourcesItem(
-                  ID_1,
-                  ImmutableList.of(
-                      SAMPLE_REPO_EXAMPLE_PATH + "/file.txt", SAMPLE_REPO_EXAMPLE_PATH + "/file2.txt"))));
-
-
-  static final TextDocumentIdentifier INVERSE_SOURCES_DOCUMENT =
-      new TextDocumentIdentifier("file://" + WORKSPACE_FULL_PATH + "/dep/Dep.scala");
-
-  static final InverseSourcesResult EXPECTED_INVERSE_SOURCES =
-      new InverseSourcesResult(ImmutableList.of(ID_2));
 }
