58c5c73ea109f1018844242201273d8c5b142132
Gerard Drozdz <gerardd@interia.pl>
2020-11-30 18:44:32 +0100

    Extracted methods and improved consistency in handleEvent

--- src/main/java/org/jetbrains/bsp/bazel/server/BepServer.java
+++ src/main/java/org/jetbrains/bsp/bazel/server/BepServer.java
@@ -93,6 +93,19 @@ public class BepServer extends PublishBuildEventGrpc.PublishBuildEventImplBase {
     return diagnosticsProtosLocations;
   }
 
+  public void collectDiagnostics(
+      Map<Uri, List<PublishDiagnosticsParams>> filesToDiagnostics,
+      BuildTargetIdentifier target,
+      String diagnosticsLocation)
+      throws IOException {
+    diagnosticsDispatcher.collectDiagnostics(filesToDiagnostics, target, diagnosticsLocation);
+  }
+
+  public void emitDiagnostics(
+      Map<Uri, List<PublishDiagnosticsParams>> filesToDiagnostics, BuildTargetIdentifier target) {
+    diagnosticsDispatcher.emitDiagnostics(filesToDiagnostics, target);
+  }
+
   public void handleEvent(BuildEvent buildEvent) {
     try {
       BuildEventStreamProtos.BuildEvent event =
@@ -100,70 +113,76 @@ public class BepServer extends PublishBuildEventGrpc.PublishBuildEventImplBase {
 
       System.out.println("Got event" + event + "\nevent-end\n");
 
-      if (event.hasStarted() && event.getStarted().getCommand().equals("build")) {
-        BuildEventStreamProtos.BuildStarted buildStarted = event.getStarted();
-
-        TaskId taskId = new TaskId(buildStarted.getUuid());
-        TaskStartParams startParams = new TaskStartParams(taskId);
-        startParams.setEventTime(buildStarted.getStartTimeMillis());
-
-        bspClient.onBuildTaskStart(startParams);
-        taskParkingLot.add(taskId);
-      }
-
-      if (event.hasFinished()) {
-        BuildEventStreamProtos.BuildFinished buildFinished = event.getFinished();
-
-        if (taskParkingLot.size() == 0) {
-          System.out.println("No start event id was found.");
-          return;
-        } else if (taskParkingLot.size() > 1) {
-          System.out.println("More than 1 start event was found");
-          return;
-        }
-
-        TaskFinishParams finishParams =
-            new TaskFinishParams(
-                taskParkingLot.pop(),
-                BepServer.convertExitCode(buildFinished.getExitCode().getCode()));
-
-        finishParams.setEventTime(buildFinished.getFinishTimeMillis());
-        bspClient.onBuildTaskFinish(finishParams);
-      }
-
       if (event.getId().hasNamedSet()) {
         namedSetsOfFiles.put(event.getId().getNamedSet().getId(), event.getNamedSetOfFiles());
       }
 
+      if (event.hasStarted() && event.getStarted().getCommand().equals("build")) {
+        processedStartedEvent(event.getStarted());
+      }
+
+      if (event.hasFinished()) {
+        processFinishedEvent(event.getFinished());
+      }
+
       if (event.hasCompleted()) {
-        processCompletionEvent(event);
+        processCompletedEvent(event.getCompleted());
       }
 
       if (event.hasAction()) {
-        processActionDiagnostics(event);
+        processActionEvent(event.getAction());
       }
 
       if (event.hasAborted()) {
-        processAbortion(event.getAborted());
+        processAbortedEvent(event.getAborted());
       }
 
       if (event.hasProgress()) {
-        BuildEventStreamProtos.Progress progress = event.getProgress();
-        processStdErrDiagnostics(progress);
-
-        String message = progress.getStderr().trim();
-        if (!message.isEmpty()) {
-          buildClientLogger.logMessage(progress.getStderr().trim());
-        }
+        processProgressEvent(event.getProgress());
       }
-
     } catch (IOException e) {
       System.err.println("Error deserializing BEP proto: " + e);
     }
   }
 
-  private void processCompletionEvent(BuildEventStreamProtos.BuildEvent event) throws IOException {
-    List<OutputGroup> outputGroups = event.getCompleted().getOutputGroupList();
+  private void processedStartedEvent(BuildEventStreamProtos.BuildStarted buildStarted) {
+    TaskId taskId = new TaskId(buildStarted.getUuid());
+    TaskStartParams startParams = new TaskStartParams(taskId);
+    startParams.setEventTime(buildStarted.getStartTimeMillis());
+
+    bspClient.onBuildTaskStart(startParams);
+    taskParkingLot.add(taskId);
+  }
+
+  private void processFinishedEvent(BuildEventStreamProtos.BuildFinished buildFinished) {
+    if (taskParkingLot.empty()) {
+      System.out.println("No start event id was found.");
+      return;
+    } else if (taskParkingLot.size() > 1) {
+      System.out.println("More than 1 start event was found");
+      return;
+    }
+
+    TaskFinishParams finishParams =
+        new TaskFinishParams(
+            taskParkingLot.pop(),
+            BepServer.convertExitCode(buildFinished.getExitCode().getCode()));
+
+    finishParams.setEventTime(buildFinished.getFinishTimeMillis());
+    bspClient.onBuildTaskFinish(finishParams);
+  }
+
+  private void processProgressEvent(BuildEventStreamProtos.Progress progress) {
+    processStdErrDiagnostics(progress);
+
+    String message = progress.getStderr().trim();
+    if (!message.isEmpty()) {
+      buildClientLogger.logMessage(progress.getStderr().trim());
+    }
+  }
+
+  private void processCompletedEvent(BuildEventStreamProtos.TargetComplete targetComplete) throws IOException {
+    List<OutputGroup> outputGroups = targetComplete.getOutputGroupList();
 
     if (outputGroups.size() == 1) {
       BuildEventStreamProtos.OutputGroup outputGroup = outputGroups.get(0);
@@ -199,9 +218,15 @@ public class BepServer extends PublishBuildEventGrpc.PublishBuildEventImplBase {
     }
   }
 
-  private void processActionDiagnostics(BuildEventStreamProtos.BuildEvent event)
+  private void processAbortedEvent(BuildEventStreamProtos.Aborted aborted) {
+    if (aborted.getReason() != BuildEventStreamProtos.Aborted.AbortReason.NO_BUILD) {
+      buildClientLogger.logError(
+          "Command aborted with reason " + aborted.getReason() + ": " + aborted.getDescription());
+    }
+  }
+
+  private void processActionEvent(BuildEventStreamProtos.ActionExecuted action)
       throws IOException {
-    BuildEventStreamProtos.ActionExecuted action = event.getAction();
     String actionType = action.getType();
 
     if (!Constants.SUPPORTED_ACTIONS.contains(actionType)) {
@@ -209,8 +234,6 @@ public class BepServer extends PublishBuildEventGrpc.PublishBuildEventImplBase {
       // TODO(illicitonion): Maybe include them as task notifications (rather than diagnostics).
       return;
     }
-
-    System.out.println("DWH: Event: " + event + "\n\n");
 
     Map<Uri, List<PublishDiagnosticsParams>> filesToDiagnostics = new HashMap<>();
     BuildTargetIdentifier target = new BuildTargetIdentifier(action.getLabel());
@@ -237,13 +260,6 @@ public class BepServer extends PublishBuildEventGrpc.PublishBuildEventImplBase {
     }
 
     diagnosticsDispatcher.emitDiagnostics(filesToDiagnostics, target);
-  }
-
-  private void processAbortion(BuildEventStreamProtos.Aborted aborted) {
-    if (aborted.getReason() != BuildEventStreamProtos.Aborted.AbortReason.NO_BUILD) {
-      buildClientLogger.logError(
-          "Command aborted with reason " + aborted.getReason() + ": " + aborted.getDescription());
-    }
   }
 
   private void processStdErrDiagnostics(BuildEventStreamProtos.Progress progress) {
@@ -304,17 +320,4 @@ public class BepServer extends PublishBuildEventGrpc.PublishBuildEventImplBase {
                     diagnostics,
                     false)));
   }
-
-  public void collectDiagnostics(
-      Map<Uri, List<PublishDiagnosticsParams>> filesToDiagnostics,
-      BuildTargetIdentifier target,
-      String diagnosticsLocation)
-      throws IOException {
-    diagnosticsDispatcher.collectDiagnostics(filesToDiagnostics, target, diagnosticsLocation);
-  }
-
-  public void emitDiagnostics(
-      Map<Uri, List<PublishDiagnosticsParams>> filesToDiagnostics, BuildTargetIdentifier target) {
-      diagnosticsDispatcher.emitDiagnostics(filesToDiagnostics, target);
-    }
 }
