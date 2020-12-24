9d6546579d39ea08b7d5a46661e6853585b67224
Abrams27 <abramowiczmarcin99@gmail.com>
2020-12-24 14:36:37 +0100

    BazelRunner logs + minor change

--- src/main/java/org/jetbrains/bsp/bazel/server/bazel/BazelRunner.java
+++ src/main/java/org/jetbrains/bsp/bazel/server/bazel/BazelRunner.java
@@ -2,12 +2,15 @@ package org.jetbrains.bsp.bazel.server.bazel;
 
 import com.google.common.collect.Lists;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Semaphore;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class BazelRunner {
+
+  private static final Logger LOGGER = LogManager.getLogger(BazelRunner.class);
 
   private static final String PUBLISH_ALL_ACTIONS = "--build_event_publish_all_actions";
   private static final String BES_BACKEND = "--bes_backend=grpc://localhost:";
@@ -31,15 +34,24 @@ public class BazelRunner {
 
   public ProcessResults runBazelCommandBes(List<String> args) {
     if (besBackendPort == null) {
+      LOGGER.fatal("BES port not set");
       throw new IllegalStateException("BES port not set");
     }
     if (args.size() < 1) {
+      LOGGER.fatal("Not enough arguments");
       throw new IllegalArgumentException("Not enough arguments");
     }
+
+    List<String> newArgs = getNewArgs(args);
+    return runBazelCommand(newArgs);
+  }
+
+  private List<String> getNewArgs(List<String> args) {
     List<String> newArgs = new ArrayList<>(args);
     newArgs.add(1, BES_BACKEND + besBackendPort);
     newArgs.add(2, PUBLISH_ALL_ACTIONS);
-    return runBazelCommand(newArgs);
+
+    return newArgs;
   }
 
   public ProcessResults runBazelCommand(String... args) {
@@ -48,22 +60,28 @@ public class BazelRunner {
 
   public ProcessResults runBazelCommand(List<String> args) {
     try {
-      List<String> processArgs = new ArrayList<>();
-      processArgs.add(bazel);
-      processArgs.addAll(args);
-      System.out.printf("Running: %s%n", processArgs); // TODO better logging
+      List<String> processArgs = getProcessArgs(args);
+      LOGGER.info("Running: {}", processArgs);
+
       processLock.acquire();
+
       ProcessBuilder processBuilder = new ProcessBuilder(processArgs);
       Process process = processBuilder.start();
-
-      InputStream os = process.getInputStream();
-      InputStream es = process.getErrorStream();
       int exitCode = process.waitFor();
+
       processLock.release();
 
       return new ProcessResults(process.getInputStream(), process.getErrorStream(), exitCode);
     } catch (InterruptedException | IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private List<String> getProcessArgs(List<String> args) {
+    List<String> processArgs = new ArrayList<>();
+    processArgs.add(bazel);
+    processArgs.addAll(args);
+
+    return processArgs;
   }
 }
