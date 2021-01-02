7329ea9e86c1be647b3bcc4735ca3ce4ce212f87
Abrams27 <abramowiczmarcin99@gmail.com>
2021-01-02 21:09:06 +0100

    refactor

--- src/main/java/org/jetbrains/bsp/bazel/server/bazel/BazelRunner.java
+++ src/main/java/org/jetbrains/bsp/bazel/server/bazel/BazelRunner.java
@@ -2,7 +2,6 @@ package org.jetbrains.bsp.bazel.server.bazel;
 
 import com.google.common.collect.Lists;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.apache.logging.log4j.LogManager;
