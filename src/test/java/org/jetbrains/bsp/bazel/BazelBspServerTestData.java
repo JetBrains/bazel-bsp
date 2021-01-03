8908c63d3cbdfa5296318fdacfc84651b0149cfb
Abrams27 <abramowiczmarcin99@gmail.com>
2021-01-03 15:32:52 +0100

    BazelBspServerTestData rollback

--- src/test/java/org/jetbrains/bsp/bazel/BazelBspServerTestData.java
+++ src/test/java/org/jetbrains/bsp/bazel/BazelBspServerTestData.java
@@ -21,8 +21,39 @@ import org.jetbrains.bsp.bazel.commons.Constants;
 
 class BazelBspServerTestData {
 
+  private static final String SAMPLE_REPO_PATH = "sample-repo";
+  private static final String SAMPLE_REPO_EXAMPLE_PATH = SAMPLE_REPO_PATH + "/example";
+  private static final String SAMPLE_REPO_DEP_PATH = SAMPLE_REPO_PATH + "/dep";
+
+  private static final String BUILD_WORKSPACE_DIRECTORY = "BUILD_WORKSPACE_DIRECTORY";
+  private static final String WORKSPACE_DIR_PATH = System.getenv(BUILD_WORKSPACE_DIRECTORY);
+
+  private static final BuildTargetIdentifier ID_1 = new BuildTargetIdentifier("//example:example");
+  private static final BuildTargetIdentifier ID_2 = new BuildTargetIdentifier("//dep:dep");
+  private static final BuildTargetIdentifier ID_3 =
+      new BuildTargetIdentifier("//dep/deeper:deeper");
+
   static final Duration TEST_CLIENT_TIMEOUT_IN_MINUTES = Duration.ofMinutes(4);
   static final Integer TEST_EXECUTION_TIMEOUT_IN_MINUTES = 15;
+
+  static final String WORKSPACE_FULL_PATH = WORKSPACE_DIR_PATH + "/" + SAMPLE_REPO_PATH;
+
+  static final WorkspaceBuildTargetsResult EXPECTED_BUILD_TARGETS =
+      new WorkspaceBuildTargetsResult(
+          ImmutableList.of(
+              new BuildTarget(
+                  ID_1,
+                  ImmutableList.of(),
+                  ImmutableList.of(Constants.SCALA),
+                  ImmutableList.of(ID_2),
+                  new BuildTargetCapabilities(true, false, true)),
+              new BuildTarget(
+                  ID_2,
+                  ImmutableList.of(),
+                  ImmutableList.of(Constants.JAVA, Constants.SCALA),
+                  ImmutableList.of(ID_3),
+                  new BuildTargetCapabilities(true, false, false))));
+
   static final List<String> DEPENDENCIES =
       ImmutableList.of(
           "https/repo1.maven.org/maven2/com/google/j2objc/j2objc-annotations/1.3/j2objc-annotations-1.3-sources.jar",
@@ -32,24 +63,7 @@ class BazelBspServerTestData {
           "https/repo1.maven.org/maven2/org/codehaus/mojo/animal-sniffer-annotations/1.17/animal-sniffer-annotations-1.17-sources.jar",
           "https/repo1.maven.org/maven2/org/checkerframework/checker-qual/2.8.1/checker-qual-2.8.1-sources.jar",
           "https/repo1.maven.org/maven2/com/google/guava/guava/28.0-jre/guava-28.0-jre-sources.jar");
-  static final TextDocumentIdentifier INVERSE_SOURCES_DOCUMENT =
-      new TextDocumentIdentifier("file://" + WORKSPACE_FULL_PATH + "/dep/Dep.scala");
-  private static final String SAMPLE_REPO_PATH = "sample-repo";
-  private static final String SAMPLE_REPO_EXAMPLE_PATH = SAMPLE_REPO_PATH + "/example";
-  private static final String SAMPLE_REPO_DEP_PATH = SAMPLE_REPO_PATH + "/dep";
-  private static final String BUILD_WORKSPACE_DIRECTORY = "BUILD_WORKSPACE_DIRECTORY";
-  private static final String WORKSPACE_DIR_PATH = System.getenv(BUILD_WORKSPACE_DIRECTORY);
-  static final String WORKSPACE_FULL_PATH = WORKSPACE_DIR_PATH + "/" + SAMPLE_REPO_PATH;
-  private static final BuildTargetIdentifier ID_1 = new BuildTargetIdentifier("//example:example");
-  static final ResourcesResult EXPECTED_RESOURCES =
-      new ResourcesResult(
-          ImmutableList.of(
-              new ResourcesItem(
-                  ID_1,
-                  ImmutableList.of(
-                      SAMPLE_REPO_EXAMPLE_PATH + "/file.txt",
-                      SAMPLE_REPO_EXAMPLE_PATH + "/file2.txt"))));
-  private static final BuildTargetIdentifier ID_2 = new BuildTargetIdentifier("//dep:dep");
+
   static final DependencySourcesResult EXPECTED_DEPENDENCIES =
       new DependencySourcesResult(
           ImmutableList.of(
@@ -75,23 +89,19 @@ class BazelBspServerTestData {
                           SAMPLE_REPO_DEP_PATH + "/JavaTest.java", SourceItemKind.FILE, false),
                       new SourceItem(
                           SAMPLE_REPO_DEP_PATH + "/Dep.scala", SourceItemKind.FILE, false)))));
+
+  static final ResourcesResult EXPECTED_RESOURCES =
+      new ResourcesResult(
+          ImmutableList.of(
+              new ResourcesItem(
+                  ID_1,
+                  ImmutableList.of(
+                      SAMPLE_REPO_EXAMPLE_PATH + "/file.txt",
+                      SAMPLE_REPO_EXAMPLE_PATH + "/file2.txt"))));
+
+  static final TextDocumentIdentifier INVERSE_SOURCES_DOCUMENT =
+      new TextDocumentIdentifier("file://" + WORKSPACE_FULL_PATH + "/dep/Dep.scala");
+
   static final InverseSourcesResult EXPECTED_INVERSE_SOURCES =
       new InverseSourcesResult(ImmutableList.of(ID_2));
-  private static final BuildTargetIdentifier ID_3 =
-      new BuildTargetIdentifier("//dep/deeper:deeper");
-  static final WorkspaceBuildTargetsResult EXPECTED_BUILD_TARGETS =
-      new WorkspaceBuildTargetsResult(
-          ImmutableList.of(
-              new BuildTarget(
-                  ID_1,
-                  ImmutableList.of(),
-                  ImmutableList.of(Constants.SCALA),
-                  ImmutableList.of(ID_2),
-                  new BuildTargetCapabilities(true, false, true)),
-              new BuildTarget(
-                  ID_2,
-                  ImmutableList.of(),
-                  ImmutableList.of(Constants.JAVA, Constants.SCALA),
-                  ImmutableList.of(ID_3),
-                  new BuildTargetCapabilities(true, false, false))));
 }
