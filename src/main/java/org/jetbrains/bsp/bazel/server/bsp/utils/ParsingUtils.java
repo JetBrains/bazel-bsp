6c7f569ef4db5a48941e971b000c3b182e51c5e1
Abrams27 <abramowiczmarcin99@gmail.com>
2021-01-05 01:00:59 +0100

    getRuleType rollback

--- src/main/java/org/jetbrains/bsp/bazel/server/bsp/utils/ParsingUtils.java
+++ src/main/java/org/jetbrains/bsp/bazel/server/bsp/utils/ParsingUtils.java
@@ -1,18 +1,9 @@
 package org.jetbrains.bsp.bazel.server.bsp.utils;
 
 import ch.epfl.scala.bsp4j.BuildTargetTag;
-import com.google.common.collect.ImmutableMap;
-import java.util.Map;
 import org.jetbrains.bsp.bazel.commons.Constants;
 
 public class ParsingUtils {
-
-  private static final Map<String, String> RULE_CLASS_TO_RULE_TYPE =
-      ImmutableMap.of(
-          Constants.LIBRARY_RULE_TYPE, BuildTargetTag.LIBRARY,
-          Constants.BINARY_RULE_TYPE, BuildTargetTag.APPLICATION,
-          Constants.TEST_RULE_TYPE, BuildTargetTag.TEST);
-  private static final String DEFAULT_RULE_TYPE = BuildTargetTag.NO_IDE;
 
   public static String convertOutputToPath(String output, String prefix) {
     String pathToFile = output.replaceAll("(//|:)", "/");
@@ -20,6 +11,18 @@ public class ParsingUtils {
   }
 
   public static String getRuleType(String ruleClass) {
-    return RULE_CLASS_TO_RULE_TYPE.getOrDefault(ruleClass, DEFAULT_RULE_TYPE);
+    if (ruleClass.contains(Constants.LIBRARY_RULE_TYPE)) {
+      return BuildTargetTag.LIBRARY;
+    }
+
+    if (ruleClass.contains(Constants.BINARY_RULE_TYPE)) {
+      return BuildTargetTag.APPLICATION;
+    }
+
+    if (ruleClass.contains(Constants.TEST_RULE_TYPE)) {
+      return BuildTargetTag.TEST;
+    }
+
+    return BuildTargetTag.NO_IDE;
   }
 }
