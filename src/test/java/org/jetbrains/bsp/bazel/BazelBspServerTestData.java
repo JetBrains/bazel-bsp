b2db9c302b69550014fc4c306bf885f0b30c4e06
Abrams27 <abramowiczmarcin99@gmail.com>
2021-02-06 20:28:45 +0100

    more descriptive targets names

--- src/test/java/org/jetbrains/bsp/bazel/BazelBspServerTestData.java
+++ src/test/java/org/jetbrains/bsp/bazel/BazelBspServerTestData.java
@@ -42,19 +42,22 @@ class BazelBspServerTestData {
   private static final String BUILD_WORKSPACE_DIRECTORY = "BUILD_WORKSPACE_DIRECTORY";
   private static final String WORKSPACE_DIR_PATH = System.getenv(BUILD_WORKSPACE_DIRECTORY);
 
-  private static final BuildTargetIdentifier ID_1 = new BuildTargetIdentifier("//example:example");
-  private static final BuildTargetIdentifier ID_2 = new BuildTargetIdentifier("//dep:dep");
-  private static final BuildTargetIdentifier ID_3 =
+  private static final BuildTargetIdentifier EXAMPLE_EXAMPLE_TARGET =
+      new BuildTargetIdentifier("//example:example");
+  private static final BuildTargetIdentifier DEP_DEP_TARGET =
+      new BuildTargetIdentifier("//dep:dep");
+  private static final BuildTargetIdentifier DEP_DEEPER_DEEPER_TARGET =
       new BuildTargetIdentifier("//dep/deeper:deeper");
-  private static final BuildTargetIdentifier ID_4 =
+  private static final BuildTargetIdentifier EXAMPLE_EXAMPLE_TEST_TARGET =
       new BuildTargetIdentifier("//example:example-test");
-  private static final BuildTargetIdentifier ID_8 = new BuildTargetIdentifier("//dep:java-dep");
-  private static final BuildTargetIdentifier ID_5 =
+  private static final BuildTargetIdentifier WITHOUT_MAIN_CLASS_LIBRARY_TARGET =
       new BuildTargetIdentifier("//target_without_main_class:library");
-  private static final BuildTargetIdentifier ID_6 =
+  private static final BuildTargetIdentifier WITHOUT_ARGS_BINARY_TARGET =
       new BuildTargetIdentifier("//target_without_args:binary");
-  private static final BuildTargetIdentifier ID_7 =
+  private static final BuildTargetIdentifier WITHOUT_JVM_FLAGS_BINARY_TARGET =
       new BuildTargetIdentifier("//target_without_jvm_flags:binary");
+  private static final BuildTargetIdentifier DEP_JAVA_DEP_TARGET =
+      new BuildTargetIdentifier("//dep:java-dep");
 
   static final Duration TEST_CLIENT_TIMEOUT_IN_MINUTES = Duration.ofMinutes(6);
   static final Integer TEST_EXECUTION_TIMEOUT_IN_MINUTES = 25;
@@ -65,16 +68,16 @@ class BazelBspServerTestData {
       new WorkspaceBuildTargetsResult(
           ImmutableList.of(
               new BuildTarget(
-                  ID_1,
+                  EXAMPLE_EXAMPLE_TARGET,
                   ImmutableList.of(),
                   ImmutableList.of(Constants.SCALA),
-                  ImmutableList.of(ID_2),
+                  ImmutableList.of(DEP_DEP_TARGET),
                   new BuildTargetCapabilities(true, false, true)),
               new BuildTarget(
-                  ID_2,
+                  DEP_DEP_TARGET,
                   ImmutableList.of(),
                   ImmutableList.of(Constants.JAVA, Constants.SCALA),
-                  ImmutableList.of(ID_3),
+                  ImmutableList.of(DEP_DEEPER_DEEPER_TARGET),
                   new BuildTargetCapabilities(true, false, false))));
 
   static final List<String> DEPENDENCIES =
@@ -90,21 +93,21 @@ class BazelBspServerTestData {
   static final DependencySourcesResult EXPECTED_DEPENDENCIES =
       new DependencySourcesResult(
           ImmutableList.of(
-              new DependencySourcesItem(ID_1, DEPENDENCIES),
-              new DependencySourcesItem(ID_2, DEPENDENCIES)));
+              new DependencySourcesItem(EXAMPLE_EXAMPLE_TARGET, DEPENDENCIES),
+              new DependencySourcesItem(DEP_DEP_TARGET, DEPENDENCIES)));
 
   static final SourcesResult EXPECTED_SOURCES =
       new SourcesResult(
           ImmutableList.of(
               new SourcesItem(
-                  ID_1,
+                  EXAMPLE_EXAMPLE_TARGET,
                   ImmutableList.of(
                       new SourceItem(
                           SAMPLE_REPO_EXAMPLE_PATH + "/Example.scala",
                           SourceItemKind.FILE,
                           false))),
               new SourcesItem(
-                  ID_2,
+                  DEP_DEP_TARGET,
                   ImmutableList.of(
                       new SourceItem(
                           SAMPLE_REPO_DEP_PATH + "/Test.scala", SourceItemKind.FILE, false),
@@ -117,7 +120,7 @@ class BazelBspServerTestData {
       new ResourcesResult(
           ImmutableList.of(
               new ResourcesItem(
-                  ID_1,
+                  EXAMPLE_EXAMPLE_TARGET,
                   ImmutableList.of(
                       SAMPLE_REPO_EXAMPLE_PATH + "/file.txt",
                       SAMPLE_REPO_EXAMPLE_PATH + "/file2.txt"))));
@@ -126,30 +129,35 @@ class BazelBspServerTestData {
       new TextDocumentIdentifier("file://" + WORKSPACE_FULL_PATH + "/dep/Dep.scala");
 
   static final InverseSourcesResult EXPECTED_INVERSE_SOURCES =
-      new InverseSourcesResult(ImmutableList.of(ID_2));
+      new InverseSourcesResult(ImmutableList.of(DEP_DEP_TARGET));
 
   static final ScalaMainClassesParams SCALA_MAIN_CLASSES_PARAMS =
-      new ScalaMainClassesParams(ImmutableList.of(ID_1, ID_5, ID_6, ID_7));
+      new ScalaMainClassesParams(
+          ImmutableList.of(
+              EXAMPLE_EXAMPLE_TARGET,
+              WITHOUT_MAIN_CLASS_LIBRARY_TARGET,
+              WITHOUT_ARGS_BINARY_TARGET,
+              WITHOUT_JVM_FLAGS_BINARY_TARGET));
 
   static final ScalaMainClassesResult EXPECTED_SCALA_MAIN_CLASSES =
       new ScalaMainClassesResult(
           ImmutableList.of(
               new ScalaMainClassesItem(
-                  ID_1,
+                  EXAMPLE_EXAMPLE_TARGET,
                   Collections.singletonList(
                       new ScalaMainClass(
                           "example.Example",
                           ImmutableList.of("arg1", "arg2"),
                           ImmutableList.of("-Xms2G -Xmx5G")))),
               new ScalaMainClassesItem(
-                  ID_6,
+                  WITHOUT_ARGS_BINARY_TARGET,
                   Collections.singletonList(
                       new ScalaMainClass(
                           "example.Example",
                           ImmutableList.of(),
                           ImmutableList.of("-Xms2G -Xmx5G")))),
               new ScalaMainClassesItem(
-                  ID_7,
+                  WITHOUT_JVM_FLAGS_BINARY_TARGET,
                   Collections.singletonList(
                       new ScalaMainClass(
                           "example.Example",
@@ -157,30 +165,39 @@ class BazelBspServerTestData {
                           ImmutableList.of())))));
 
   static final ScalaTestClassesParams SCALA_TEST_CLASSES_PARAMS =
-      new ScalaTestClassesParams(ImmutableList.of(ID_1, ID_4));
+      new ScalaTestClassesParams(
+          ImmutableList.of(EXAMPLE_EXAMPLE_TARGET, EXAMPLE_EXAMPLE_TEST_TARGET));
 
   static final ScalaTestClassesResult EXPECTED_SCALA_TEST_CLASSES =
       new ScalaTestClassesResult(
           ImmutableList.of(
-              new ScalaTestClassesItem(ID_4, ImmutableList.of("example.ExampleTest"))));
+              new ScalaTestClassesItem(
+                  EXAMPLE_EXAMPLE_TEST_TARGET, ImmutableList.of("example.ExampleTest"))));
 
   static final JavacOptionsParams JAVAC_OPTIONS_PARAMS =
-      new JavacOptionsParams(ImmutableList.of(ID_1, ID_8));
+      new JavacOptionsParams(ImmutableList.of(EXAMPLE_EXAMPLE_TARGET, DEP_JAVA_DEP_TARGET));
 
   static final JavacOptionsResult EXPECTED_JAVAC_OPTIONS =
       new JavacOptionsResult(
           ImmutableList.of(
-              new JavacOptionsItem(ID_1, ImmutableList.of(), ImmutableList.of(), ""),
               new JavacOptionsItem(
-                  ID_8, ImmutableList.of("-Werror", "-Xlint:all"), ImmutableList.of(), "")));
+                  EXAMPLE_EXAMPLE_TARGET, ImmutableList.of(), ImmutableList.of(), ""),
+              new JavacOptionsItem(
+                  DEP_JAVA_DEP_TARGET,
+                  ImmutableList.of("-Werror", "-Xlint:all"),
+                  ImmutableList.of(),
+                  "")));
 
   static final ScalacOptionsParams SCALAC_OPTIONS_PARAMS =
-      new ScalacOptionsParams(ImmutableList.of(ID_1, ID_2));
+      new ScalacOptionsParams(ImmutableList.of(EXAMPLE_EXAMPLE_TARGET, DEP_DEP_TARGET));
 
   static final ScalacOptionsResult EXPECTED_SCALAC_OPTIONS =
       new ScalacOptionsResult(
           ImmutableList.of(
               new ScalacOptionsItem(
-                  ID_1, ImmutableList.of("-target:jvm-1.8"), ImmutableList.of(), ""),
-              new ScalacOptionsItem(ID_2, ImmutableList.of(), ImmutableList.of(), "")));
+                  EXAMPLE_EXAMPLE_TARGET,
+                  ImmutableList.of("-target:jvm-1.8"),
+                  ImmutableList.of(),
+                  ""),
+              new ScalacOptionsItem(DEP_DEP_TARGET, ImmutableList.of(), ImmutableList.of(), "")));
 }
