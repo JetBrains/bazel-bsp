858430dedb012cf2c149622d5242348ba57f0c1b
Andre Rocha <andrefmrocha@live.com.pt>
2021-03-03 15:39:52 +0000

    Refactor semantic version comparator

--- src/main/java/org/jetbrains/bsp/bazel/server/bazel/data/SemanticVersion.java
+++ src/main/java/org/jetbrains/bsp/bazel/server/bazel/data/SemanticVersion.java
@@ -1,14 +1,23 @@
 package org.jetbrains.bsp.bazel.server.bazel.data;
 
+import java.util.Comparator;
+
 public class SemanticVersion implements Comparable<SemanticVersion> {
 
   private static final String SEMANTIC_VERSION_REGEX = "[0-9]+(\\.[0-9]+)*";
   private static final int MAJOR_VERSION_LOCATION = 0;
   private static final int MINOR_VERSION_LOCATION = 1;
-  private static final int PATCH_VERSION_LOCATION = 1;
+  private static final int PATCH_VERSION_LOCATION = 2;
+  private final Comparator<SemanticVersion> comparator =
+      Comparator.nullsLast(
+          Comparator.comparing(SemanticVersion::getMajorVersion)
+              .thenComparing(SemanticVersion::getMinorVersion)
+              .thenComparing(SemanticVersion::getPatchVersion));
 
   private final String version;
-  private final String[] versionParts;
+  private final int majorVersion;
+  private final int minorVersion;
+  private final int patchVersion;
 
   public SemanticVersion(String version) {
     if (version == null) {
@@ -24,44 +33,30 @@ public class SemanticVersion implements Comparable<SemanticVersion> {
     }
 
     this.version = version;
-    this.versionParts = parts;
+    this.majorVersion = Integer.parseInt(parts[MAJOR_VERSION_LOCATION]);
+    this.minorVersion = Integer.parseInt(parts[MINOR_VERSION_LOCATION]);
+    this.patchVersion = Integer.parseInt(parts[PATCH_VERSION_LOCATION]);
+  }
+
+  public static SemanticVersion fromReleaseData(String version) {
+    return new SemanticVersion(version.split(" ")[1]);
   }
 
   public final int getMajorVersion() {
-    return Integer.parseInt(versionParts[MAJOR_VERSION_LOCATION]);
+    return majorVersion;
   }
 
   public final int getMinorVersion() {
-    return Integer.parseInt(versionParts[MINOR_VERSION_LOCATION]);
+    return minorVersion;
   }
 
   public final int getPatchVersion() {
-    return Integer.parseInt(versionParts[PATCH_VERSION_LOCATION]);
-  }
-
-  static SemanticVersion fromReleaseData(String version) {
-    return new SemanticVersion(version.split(" ")[1]);
+    return patchVersion;
   }
 
   @Override
   public int compareTo(SemanticVersion other) {
-    if (other == null) return 1;
-    int thisMajorVersion = this.getMajorVersion();
-    int thatMajorVersion = other.getMajorVersion();
-    if (thisMajorVersion < thatMajorVersion) return -1;
-    if (thisMajorVersion > thatMajorVersion) return 1;
-
-    int thisMinorVersion = this.getMinorVersion();
-    int thatMinorVersion = other.getMinorVersion();
-    if (thisMinorVersion < thatMinorVersion) return -1;
-    if (thisMinorVersion > thatMinorVersion) return 1;
-
-    int thisPatchVersion = this.getPatchVersion();
-    int thatPatchVersion = other.getPatchVersion();
-    if (thisPatchVersion < thatPatchVersion) return -1;
-    if (thisPatchVersion > thatPatchVersion) return 1;
-
-    return 0;
+    return comparator.compare(this, other);
   }
 
   @Override
