775001880870f64e13665a8f2393a988a5e6481a
Marcin Abramowicz <Marcin.Abramowicz@jetbrains.com>
2021-09-22 12:41:32 +0200

    fix

--- e2e/src/main/java/org/jetbrains/bsp/bazel/BazelBspSampleRepoTest.java
+++ e2e/src/main/java/org/jetbrains/bsp/bazel/BazelBspSampleRepoTest.java
@@ -218,13 +218,13 @@ public class BazelBspSampleRepoTest extends BazelBspTestBaseScenario {
             ImmutableList.of("example.ExampleTest"));
 
     // TODO (https://github.com/JetBrains/bazel-bsp/issues/96)
-    // ScalaTestClassesItem exampleExampleSpec2TestTestClasses =
-    //    new ScalaTestClassesItem(
-    //        new BuildTargetIdentifier("//example:example-spec2-test"),
-    //        ImmutableList.of("example.ExampleSpec2Test"));
+    ScalaTestClassesItem exampleExampleSpec2TestTestClasses =
+        new ScalaTestClassesItem(
+            new BuildTargetIdentifier("//example:example-spec2-test"), ImmutableList.of());
 
     ScalaTestClassesResult expectedScalaTestClassesResult =
-        new ScalaTestClassesResult(ImmutableList.of(exampleExampleTestTestClasses));
+        new ScalaTestClassesResult(
+            ImmutableList.of(exampleExampleTestTestClasses, exampleExampleSpec2TestTestClasses));
 
     return new BazelBspTestScenarioStep(
         "Scala test classes",
