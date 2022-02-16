ee4e672887088a7e9677495eccd31e39451d48ac
odisseus <odisseus@users.noreply.github.com>
2022-02-16 07:59:14 +0100

    Store BazelProcess output inside BazelProcessResult

--- bazelrunner/src/main/java/org/jetbrains/bsp/bazel/bazelrunner/data/BazelProcessResult.java
+++ bazelrunner/src/main/java/org/jetbrains/bsp/bazel/bazelrunner/data/BazelProcessResult.java
@@ -3,20 +3,23 @@ package org.jetbrains.bsp.bazel.bazelrunner.data;
 import ch.epfl.scala.bsp4j.StatusCode;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 import org.jetbrains.bsp.bazel.bazelrunner.utils.BazelStreamReader;
 import org.jetbrains.bsp.bazel.commons.ExitCodeMapper;
+import org.jetbrains.bsp.bazel.commons.Lazy;
 
 public class BazelProcessResult {
 
   private static final String LINES_DELIMITER = "\n";
 
-  private final InputStream stdout;
-  private final InputStream stderr;
+  private final StreamStorage stdout;
+  private final StreamStorage stderr;
   private final int exitCode;
 
   public BazelProcessResult(InputStream stdout, InputStream stderr, int exitCode) {
-    this.stdout = stdout;
-    this.stderr = stderr;
+    this.stdout = new StreamStorage(stdout);
+    this.stderr = new StreamStorage(stderr);
     this.exitCode = exitCode;
   }
 
@@ -25,11 +28,36 @@ public class BazelProcessResult {
   }
 
   public List<String> getStdout() {
-    return BazelStreamReader.drainStream(stdout);
+    return stdout.getValue().get();
   }
 
   public String getJoinedStderr() {
-    List<String> lines = BazelStreamReader.drainStream(stderr);
+    List<String> lines = stderr.getValue().get();
     return String.join(LINES_DELIMITER, lines);
   }
+
+  private static class StreamStorage extends Lazy<List<String>> {
+
+    // TODO make this a weak reference?
+    private final InputStream input;
+
+    public StreamStorage(InputStream input) {
+      this.input = input;
+    }
+
+    private Optional<List<String>> supply() {
+      return Optional.of(BazelStreamReader.drainStream(input));
+    }
+
+    @Override
+    protected Supplier<Optional<List<String>>> calculateValue() {
+      return this::supply;
+    }
+
+    @Override
+    public void recalculateValue() {
+      // Do nothing. The original input stream is probably drained at this point.
+    }
+  }
+
 }
