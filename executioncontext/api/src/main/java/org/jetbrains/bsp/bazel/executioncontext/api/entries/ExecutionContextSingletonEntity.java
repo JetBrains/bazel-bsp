1f3efe72397de40e3c28484ba347c4e859fe8f2a
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2022-03-03 17:32:32 +0100

    format

--- executioncontext/api/src/main/java/org/jetbrains/bsp/bazel/executioncontext/api/entries/ExecutionContextSingletonEntity.java
+++ executioncontext/api/src/main/java/org/jetbrains/bsp/bazel/executioncontext/api/entries/ExecutionContextSingletonEntity.java
@@ -1,8 +1,8 @@
 package org.jetbrains.bsp.bazel.executioncontext.api.entries;
 
 /**
- * Base single-value <code>ExecutionContext</code> entity class - you need to extend it if you want to
- * create your single-value entity.
+ * Base single-value <code>ExecutionContext</code> entity class - you need to extend it if you want
+ * to create your single-value entity.
  */
 public abstract class ExecutionContextSingletonEntity<T> extends ExecutionContextEntity {
 
