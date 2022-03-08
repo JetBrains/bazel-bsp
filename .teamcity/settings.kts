cd29897f64782ca9d7e169cc211f84d27aad1241
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2022-03-08 15:09:30 +0100

    config 8

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -83,4 +83,6 @@ object BuildTheProject : BuildType({
 object BazelBspVcs : GitVcsRoot({
     name = "bazel-bsp"
     url = "https://github.com/JetBrains/bazel-bsp.git"
+    branch = "master"
+    branchSpec = "refs/heads/(*)"
 })
