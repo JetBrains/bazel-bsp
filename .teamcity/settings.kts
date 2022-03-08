df99d3be03833a3ef2e9c60785cea6eb9c7031f2
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2022-03-08 11:01:24 +0100

    teamcity config 4

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -28,7 +28,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2021.2"
+version = "2018.2"
 
 project {
     vcsRoot(BazelBspVcs)
