24b749ce5e0e26c89c8309ea75637d589cdda998
Basak Kapusuzoglu <basak.kapusuzoglu@jetbrains.com>
2022-03-10 16:52:28 +0100

    java path entity mapper implementation

---
+++ executioncontext/installationcontext/src/test/java/org/jetbrains/bsp/bazel/installationcontext/entities/mappers/InstallationContextJavaPathEntityMapperTest.java
@@ -0,0 +1,96 @@
+package org.jetbrains.bsp.bazel.installationcontext.entities.mappers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.file.Paths;
+import java.util.Optional;
+import org.jetbrains.bsp.bazel.executioncontext.api.entries.mappers.ProjectViewToExecutionContextEntityMapperException;
+import org.jetbrains.bsp.bazel.installationcontext.entities.InstallationContextJavaPathEntity;
+import org.jetbrains.bsp.bazel.projectview.model.ProjectView;
+import org.jetbrains.bsp.bazel.projectview.model.sections.ProjectViewJavaPathSection;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InstallationContextJavaPathEntityMapperTest {
+
+  private InstallationContextJavaPathEntityMapper mapper;
+
+  @Before
+  public void beforeEach() {
+    // given
+    this.mapper = new InstallationContextJavaPathEntityMapper();
+  }
+
+  //  @Test
+  //  public void shouldReturnFailureIfJavaPathDoesNotExist() {
+  //    // given & when
+  //    var javaPathTry = InstallationContext.builder().debuggerAddress(Option.none()).build();
+  //
+  //    // then
+  //    assertTrue(javaPathTry.isFailure());
+  //    assertEquals(IllegalStateException.class, javaPathTry.getCause().getClass());
+  //    assertEquals("Failed, 'javaPath' should be defined.", javaPathTry.getCause().getMessage());
+  //
+
+  //  @Test
+  //  public void shouldReturnFailureIfJavaPathIsNotExecutable() {}
+
+  @Test
+  public void shouldReturnSuccessWithJavaPathFromProjectViewIfJavaPathIsSpecifiedInProjectView() {
+    // given
+    var projectView =
+        ProjectView.builder()
+            .javaPath(Optional.of(new ProjectViewJavaPathSection(Paths.get("/path/to/java"))))
+            .build()
+            .get();
+
+    // when
+    var javaPathTry = mapper.map(projectView);
+
+    // then
+    assertThat(javaPathTry.isSuccess()).isTrue();
+    var javaPath = javaPathTry.get();
+
+    var expectedJavaPath = new InstallationContextJavaPathEntity(Paths.get("/path/to/java"));
+    assertThat(javaPath).isEqualTo(expectedJavaPath);
+  }
+
+  @Test
+  public void
+      shouldReturnSuccessWithJavaPathFromSystemPropertyIfJavaPathIsNotSpecifiedInProjectView() {
+    // given
+    var projectView = ProjectView.builder().javaPath(Optional.empty()).build().get();
+
+    System.setProperty("java.home", "/path/to/java");
+
+    // when
+    var javaPathTry = mapper.map(projectView);
+
+    // then
+    assertThat(javaPathTry.isSuccess()).isTrue();
+    var javaPath = javaPathTry.get();
+
+    var expectedJavaPath = new InstallationContextJavaPathEntity(Paths.get("/path/to/java"));
+    assertThat(javaPath).isEqualTo(expectedJavaPath);
+  }
+
+  @Test
+  public void
+      shouldReturnFailureIfJavaPathIsNotSpecifiedInProjectViewAndItIsImpossibleToObtainItFromSystemPropertyis() {
+    // given
+    var projectView = ProjectView.builder().javaPath(Optional.empty()).build().get();
+
+    System.clearProperty("java.home");
+
+    // when
+    var javaPathTry = mapper.map(projectView);
+
+    // then
+    assertThat(javaPathTry.isFailure()).isTrue();
+    assertThat(javaPathTry.getCause().getClass())
+        .isEqualTo(ProjectViewToExecutionContextEntityMapperException.class);
+    assertThat(javaPathTry.getCause().getMessage())
+        .isEqualTo(
+            "Mapping project view into 'java path' failed! System property 'java.home' is not specified.");
+  }
+}
