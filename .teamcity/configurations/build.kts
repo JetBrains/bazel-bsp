5850cff9158459034f7f66d98a04f05e198630c3
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2022-03-14 10:59:21 +0100

    bazel step

--- .teamcity/configurations/build.kts
+++ .teamcity/configurations/build.kts
@@ -1,17 +1,14 @@
 package configurations
 
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.ScriptBuildStep
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.bazel
 
 open class BazelBspBuildBuildType(name: String, moduleLabel: String) : BaseConfiguration.BazelBspBaseBuildType(
     name = "[build] $name",
     steps = {
-        script {
+        bazel {
             this.name = "building $moduleLabel"
-            this.scriptContent = """bazel build $moduleLabel"""
-            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-            dockerPull = true
-            dockerImage = "andrefmrocha/bazelisk"
+            this.command = "build"
+            this.targets = moduleLabel
         }
     }
 )
