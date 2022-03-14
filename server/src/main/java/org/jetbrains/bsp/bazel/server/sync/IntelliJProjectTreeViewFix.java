0b7c3fd36745fbd9989fd0a72d5f60fca3afc5db
Łukasz Wawrzyk <lukasz.wawrzyk@gmail.com>
2022-03-14 16:25:35 +0100

    [change] fake roots from target specs (#190)
    Co-authored-by: Lukasz Wawrzyk <uwawrzyk@twitter.com>

--- server/src/main/java/org/jetbrains/bsp/bazel/server/sync/IntelliJProjectTreeViewFix.java
+++ server/src/main/java/org/jetbrains/bsp/bazel/server/sync/IntelliJProjectTreeViewFix.java
@@ -1,10 +1,13 @@
 package org.jetbrains.bsp.bazel.server.sync;
 
+import ch.epfl.scala.bsp4j.BuildTargetIdentifier;
 import io.vavr.collection.HashSet;
 import io.vavr.collection.List;
 import io.vavr.collection.Set;
 import io.vavr.control.Option;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import org.jetbrains.bsp.bazel.projectview.model.ProjectView;
@@ -19,14 +22,12 @@ public class IntelliJProjectTreeViewFix {
     if (isFullWorkspaceImport(projectView)) {
       return createWorkspaceRootModule(workspaceRoot, modules);
     } else {
-      return createMultipleModules(workspaceRoot, modules);
+      return createMultipleModules(projectView, workspaceRoot, modules);
     }
   }
 
   private boolean isFullWorkspaceImport(ProjectView projectView) {
-    return projectView.getTargets().stream()
-        .flatMap(s -> s.getIncludedValues().stream())
-        .anyMatch(s -> s.getUri().startsWith("//..."));
+    return importTargetSpecs(projectView).exists(s -> s.startsWith("//..."));
   }
 
   private List<Module> createWorkspaceRootModule(URI workspaceRoot, List<Module> modules) {
@@ -39,9 +40,10 @@ public class IntelliJProjectTreeViewFix {
     return List.of(rootModule);
   }
 
-  private List<Module> createMultipleModules(URI workspaceRoot, List<Module> modules) {
+  private List<Module> createMultipleModules(
+      ProjectView projectView, URI workspaceRoot, List<Module> modules) {
     var existingRootDirectories = resolveExistingRootDirectories(modules);
-    var expectedRootDirs = resolveExpectedRootDirs(modules);
+    var expectedRootDirs = resolveExpectedRootDirs(projectView, workspaceRoot);
     return expectedRootDirs.flatMap(
         root -> {
           if (existingRootDirectories.contains(root)) {
@@ -58,9 +60,13 @@ public class IntelliJProjectTreeViewFix {
     return modules.toSet().map(Module::sourceSet).flatMap(SourceSet::sourceRoots);
   }
 
-  private List<URI> resolveExpectedRootDirs(List<Module> modules) {
-    var dirs = modules.map(Module::baseDirectory).map(URI::toString).sorted();
+  private List<URI> resolveExpectedRootDirs(ProjectView projectView, URI workspaceRoot) {
+    var dirs = rootDirsFromTargetSpecs(projectView, workspaceRoot).sorted();
     if (dirs.isEmpty()) return List.empty();
+    return removeAlreadyIncludedSubdirs(dirs);
+  }
+
+  private List<URI> removeAlreadyIncludedSubdirs(List<String> dirs) {
     var buffer = new ArrayList<String>();
     var current = dirs.get(0);
     buffer.add(current);
@@ -71,6 +77,43 @@ public class IntelliJProjectTreeViewFix {
       }
     }
     return List.ofAll(buffer.stream().map(URI::create));
+  }
+
+  private List<String> rootDirsFromTargetSpecs(ProjectView projectView, URI workspaceRoot) {
+    var root = Paths.get(workspaceRoot);
+    return importTargetSpecs(projectView)
+        .map(s -> stripSuffixes(s, ":all", "...", "/"))
+        .map(s -> stripPrefixes(s, "//"))
+        .map(root::resolve)
+        .filter(Files::exists)
+        .map(Path::toUri)
+        .map(URI::toString);
+  }
+
+  private String stripSuffixes(String s, String... suffixes) {
+    for (var suffix : suffixes) {
+      if (s.endsWith(suffix)) {
+        s = s.substring(0, s.length() - suffix.length());
+      }
+    }
+    return s;
+  }
+
+  private String stripPrefixes(String s, String... prefixes) {
+    for (var prefix : prefixes) {
+      if (s.startsWith(prefix)) {
+        s = s.substring(prefix.length());
+      }
+    }
+    return s;
+  }
+
+  private List<String> importTargetSpecs(ProjectView projectView) {
+    var stream =
+        projectView.getTargets().stream()
+            .flatMap(s -> s.getIncludedValues().stream())
+            .map(BuildTargetIdentifier::getUri);
+    return List.ofAll(stream);
   }
 
   private Module syntheticModule(String moduleName, URI baseDirectory) {
