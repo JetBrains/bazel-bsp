c2c59f1ce94f2199294e476c15dd090b83a74ca5
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2022-04-07 15:48:31 +0200

    project view parser impl is in kotlin

--- executioncontext/projectview/src/main/java/org/jetbrains/bsp/bazel/projectview/parser/ProjectViewParser.kt
+++ executioncontext/projectview/src/main/java/org/jetbrains/bsp/bazel/projectview/parser/ProjectViewParser.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.bsp.bazel.projectview.parser;
+package org.jetbrains.bsp.bazel.projectview.parser
 
-import io.vavr.control.Try;
-import java.nio.file.Path;
-import org.jetbrains.bsp.bazel.projectview.model.ProjectView;
+import io.vavr.control.Try
+import org.jetbrains.bsp.bazel.projectview.model.ProjectView
+import java.nio.file.Path
 
 /**
  * Project view file parser. Its purpose is to parse *.bazelproject file and create an instance of
@@ -10,83 +10,101 @@ import org.jetbrains.bsp.bazel.projectview.model.ProjectView;
  *
  * @see org.jetbrains.bsp.bazel.projectview.model.ProjectView
  */
-public interface ProjectViewParser {
+interface ProjectViewParser {
+    /**
+     * Parses file under `projectViewFilePath` using file under `
+     * defaultProjectViewFilePath` as a default.
+     *
+     * @param projectViewFilePath path to file with project view
+     * @param defaultProjectViewFilePath path to file with default project view
+     * @return
+     *
+     * `Try.success` with `ProjectView` if parsing has finished with
+     * success, it means:
+     *
+     * 1) files under `projectViewFilePath` and `defaultProjectViewFilePath
+    ` *  were successfully parsed (not all values have to be provided -- some fields in
+     * `ProjectView` might be `Optional.empty`). <br></br>
+     * File under `projectViewFilePath` can contain all values, then `
+     * defaultProjectViewFilePath` won't be used, or file under `projectViewFilePath
+    ` *  can be empty, then all values from file under `defaultProjectViewFilePath
+    ` *  will be used, any other configuration is possible as well.
+     *
+     * 2) file under `projectViewFilePath` doesn't exist, then all values from
+     * `defaultProjectViewFilePath` will be used.<br></br>
+     * <br></br>
+     *
+     * `Try.failure` with if:
+     *
+     * 1) file under `defaultProjectViewFilePath` doesn't exist
+     *
+     * 2) any other fail happen
+     */
+    fun parse(projectViewFilePath: Path, defaultProjectViewFilePath: Path): Try<ProjectView>
 
-  /**
-   * Parses file under <code>projectViewFilePath</code> using file under <code>
-   * defaultProjectViewFilePath</code> as a default.
-   *
-   * @param projectViewFilePath path to file with project view
-   * @param defaultProjectViewFilePath path to file with default project view
-   * @return
-   *     <p><code>Try.success</code> with <code>ProjectView</code> if parsing has finished with
-   *     success, it means:
-   *     <p>1) files under <code>projectViewFilePath</code> and <code>defaultProjectViewFilePath
-   *     </code> were successfully parsed (not all values have to be provided -- some fields in
-   *     <code>ProjectView</code> might be <code>Optional.empty</code>). <br>
-   *     File under <code>projectViewFilePath</code> can contain all values, then <code>
-   *     defaultProjectViewFilePath</code> won't be used, or file under <code>projectViewFilePath
-   *     </code> can be empty, then all values from file under <code>defaultProjectViewFilePath
-   *     </code> will be used, any other configuration is possible as well.
-   *     <p>2) file under <code>projectViewFilePath</code> doesn't exist, then all values from
-   *     <code>defaultProjectViewFilePath</code> will be used.<br>
-   *     <br>
-   *     <p><code>Try.failure</code> with if:
-   *     <p>1) file under <code>defaultProjectViewFilePath</code> doesn't exist
-   *     <p>2) any other fail happen
-   */
-  Try<ProjectView> parse(Path projectViewFilePath, Path defaultProjectViewFilePath);
+    /**
+     * Parses `projectViewFileContent` using `defaultProjectViewFileContent` as
+     * a default.
+     *
+     * @param projectViewFileContent string with project view
+     * @param defaultProjectViewFileContent string with default project view
+     * @return
+     *
+     * `Try.success` with `ProjectView` if parsing has finished with
+     * success, it means:
+     *
+     * 1) `projectViewFileContent` and `defaultProjectViewFileContent
+    ` *  were successfully parsed (not all values have to be provided -- some fields in
+     * `ProjectView` might be `Optional.empty`). <br></br>
+     * `projectViewFileContent` can contain all values, then `
+     * defaultProjectViewFileContent` won't be used, `projectViewFileContent
+    ` *  can be empty, then all values from `defaultProjectViewFileContent` will
+     * be used, any other configuration is possible as well.<br></br>
+     * <br></br>
+     *
+     * `Try.failure` with if:
+     *
+     * 1) any fail happen
+     */
+    fun parse(projectViewFileContent: String, defaultProjectViewFileContent: String): Try<ProjectView>
 
-  /**
-   * Parses <code>projectViewFileContent</code> using <code>defaultProjectViewFileContent</code> as
-   * a default.
-   *
-   * @param projectViewFileContent string with project view
-   * @param defaultProjectViewFileContent string with default project view
-   * @return
-   *     <p><code>Try.success</code> with <code>ProjectView</code> if parsing has finished with
-   *     success, it means:
-   *     <p>1) <code>projectViewFileContent</code> and <code>defaultProjectViewFileContent
-   *     </code> were successfully parsed (not all values have to be provided -- some fields in
-   *     <code>ProjectView</code> might be <code>Optional.empty</code>). <br>
-   *     <code>projectViewFileContent</code> can contain all values, then <code>
-   *     defaultProjectViewFileContent</code> won't be used, <code>projectViewFileContent
-   *     </code> can be empty, then all values from <code>defaultProjectViewFileContent</code> will
-   *     be used, any other configuration is possible as well.<br>
-   *     <br>
-   *     <p><code>Try.failure</code> with if:
-   *     <p>1) any fail happen
-   */
-  Try<ProjectView> parse(String projectViewFileContent, String defaultProjectViewFileContent);
+    /**
+     * Parses file under `projectViewFilePath`.
+     *
+     * @param projectViewFilePath path to file with project view
+     * @return
+     *
+     * `Try.success` with `ProjectView` if parsing has finished with
+     * success, it means:
+     *
+     * 1) file under `projectViewFilePath` was successfully parsed (not all values
+     * have to be provided -- some fields in `ProjectView` might be `
+     * Optional.empty`). <br></br>
+     *
+     * `Try.failure` with if:
+     *
+     * 1) file under `projectViewFilePath` doesn't exist
+     *
+     * 2) any other fail happen
+     */
+    fun parse(projectViewFilePath: Path): Try<ProjectView>
 
-  /**
-   * Parses file under <code>projectViewFilePath</code>.
-   *
-   * @param projectViewFilePath path to file with project view
-   * @return
-   *     <p><code>Try.success</code> with <code>ProjectView</code> if parsing has finished with
-   *     success, it means:
-   *     <p>1) file under <code>projectViewFilePath</code> was successfully parsed (not all values
-   *     have to be provided -- some fields in <code>ProjectView</code> might be <code>
-   *     Optional.empty</code>). <br>
-   *     <p><code>Try.failure</code> with if:
-   *     <p>1) file under <code>projectViewFilePath</code> doesn't exist
-   *     <p>2) any other fail happen
-   */
-  Try<ProjectView> parse(Path projectViewFilePath);
-
-  /**
-   * Parses <code>projectViewFileContent</code>.
-   *
-   * @param projectViewFileContent string with project view
-   * @return
-   *     <p><code>Try.success</code> with <code>ProjectView</code> if parsing has finished with
-   *     success, it means:
-   *     <p>1) <code>projectViewFileContent</code> was successfully parsed (not all values have to
-   *     be provided -- some fields in <code>ProjectView</code> might be <code>Optional.empty
-   *     </code>). <br>
-   *     <p><code>Try.failure</code> with if:
-   *     <p>1) any fail happen
-   */
-  Try<ProjectView> parse(String projectViewFileContent);
+    /**
+     * Parses `projectViewFileContent`.
+     *
+     * @param projectViewFileContent string with project view
+     * @return
+     *
+     * `Try.success` with `ProjectView` if parsing has finished with
+     * success, it means:
+     *
+     * 1) `projectViewFileContent` was successfully parsed (not all values have to
+     * be provided -- some fields in `ProjectView` might be `Optional.empty
+    ` * ). <br></br>
+     *
+     * `Try.failure` with if:
+     *
+     * 1) any fail happen
+     */
+    fun parse(projectViewFileContent: String): Try<ProjectView>
 }
