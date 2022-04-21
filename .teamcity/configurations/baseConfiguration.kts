75939eaec1b8ddbd886094bb883f2fb0b4da7b97
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2022-04-21 18:14:04 +0200

    release step

--- .teamcity/configurations/baseConfiguration.kts
+++ .teamcity/configurations/baseConfiguration.kts
@@ -3,16 +3,21 @@ package configurations
 import jetbrains.buildServer.configs.kotlin.v10.toExtId
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildSteps
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.bazel
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.ParametrizedWithType
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-open class BaseBuildType(name: String, steps: BuildSteps.() -> Unit, artifactRules: String = "") : BuildType({
+open class BaseBuildType(
+    name: String,
+    steps: BuildSteps.() -> Unit,
+    params: ParametrizedWithType.() -> Unit = { },
+    artifactRules: String = ""
+) : BuildType({
     id(name.toExtId())
     this.name = name
     this.artifactRules = artifactRules
     this.steps(steps)
+
+    this.params(params)
 
     vcs {
         root(BazelBspVcs)
--- .teamcity/configurations/release.kts
+++ .teamcity/configurations/release.kts
@@ -5,9 +5,24 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 open class ReleaseBuildType(name: String) : BaseConfiguration.BaseBuildType(
     name = "[release] $name",
+    params = {
+        password("gpg_secret", "credentialsJSON:bd309ca0-3387-4a6b-a185-95d162209e1e")
+    },
     steps = {
         script {
-            this.scriptContent = """echo XD"""
+            this.scriptContent = """
+                apt-get update
+                apt-get gpg
+                apt-get install -y python3-pip
+                pip3 install lxml
+                wget https://github.com/bazelbuild/bazelisk/releases/download/v1.11.0/bazelisk-linux-amd64
+                chmod +x bazelisk-linux-amd64
+                echo %gpg_secret% | base64 -di | gpg --import
+                bazelisk-linux-amd64 run --stamp \
+                  --define "maven_user=%jetbrains.sonatype.access.token.username%" \
+                  --define "maven_password=%jetbrains.sonatype.access.token.password%" \
+                  //server/src/main/java/org/jetbrains/bsp/bazel:bsp.publish
+            """.trimIndent()
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
             dockerPull = true
             dockerImage = "ubuntu"
