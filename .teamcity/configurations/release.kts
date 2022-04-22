b61c522848b50f7a8699e6612b680a2e95c885e1
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2022-04-22 15:44:39 +0200

    release step fix 8

--- .teamcity/configurations/release.kts
+++ .teamcity/configurations/release.kts
@@ -16,12 +16,9 @@ open class ReleaseBuildType(name: String) : BaseConfiguration.BaseBuildType(
                 pip3 install lxml
                 wget https://github.com/bazelbuild/bazelisk/releases/download/v1.11.0/bazelisk-linux-amd64
                 chmod +x bazelisk-linux-amd64
-                echo %env.PGP_SECRET% | base64 -di | gpg --import
                 ./bazelisk-linux-amd64 build //...
-                ./bazelisk-linux-amd64 run --stamp \
-                  --define "maven_user=%jetbrains.sonatype.access.token.username%" \
-                  --define "maven_password=%jetbrains.sonatype.access.token.password%" \
-                  //server/src/main/java/org/jetbrains/bsp/bazel:bsp.publish
+                
+                exit 127
             """.trimIndent()
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
             dockerPull = true
