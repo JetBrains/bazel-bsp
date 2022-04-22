e5b2ebea683e708d753f1779359eeddc3bf3c932
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2022-04-22 16:24:18 +0200

    release step fix 12

--- .teamcity/configurations/release.kts
+++ .teamcity/configurations/release.kts
@@ -15,8 +15,11 @@ open class ReleaseBuildType(name: String) : BaseConfiguration.BaseBuildType(
                 apt-get install -y wget
                 pip3 install lxml
                 bazel build //...
-                
-                exit 127
+                echo %env.PGP_SECRET% | base64 -di | gpg --import
+                bazel run --stamp \
+                  --define "maven_user=%jetbrains.sonatype.access.token.username%" \
+                  --define "maven_password=%jetbrains.sonatype.access.token.password%" \
+                  //server/src/main/java/org/jetbrains/bsp/bazel:bsp.publish
             """.trimIndent()
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
             dockerPull = true
