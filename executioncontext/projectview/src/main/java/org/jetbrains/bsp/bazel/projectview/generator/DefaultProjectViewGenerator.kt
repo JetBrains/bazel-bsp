02a7eb3a99eed1b3569a172871df3b832766160d
Basak Kapusuzoglu <basak.kapusuzoglu@jetbrains.com>
2022-05-10 16:58:25 +0200

    ProjectViewSingletonSectionGenerator, DefaultProjectViewGenerator and ProjectView updated

--- executioncontext/projectview/src/main/java/org/jetbrains/bsp/bazel/projectview/generator/DefaultProjectViewGenerator.kt
+++ executioncontext/projectview/src/main/java/org/jetbrains/bsp/bazel/projectview/generator/DefaultProjectViewGenerator.kt
@@ -1,11 +1,7 @@
 package org.jetbrains.bsp.bazel.projectview.generator
 
 import io.vavr.control.Try
-import org.jetbrains.bsp.bazel.projectview.generator.sections.ProjectViewBazelPathSectionGenerator
-import org.jetbrains.bsp.bazel.projectview.generator.sections.ProjectViewBuildFlagsSectionGenerator
-import org.jetbrains.bsp.bazel.projectview.generator.sections.ProjectViewDebuggerAddressSectionGenerator
-import org.jetbrains.bsp.bazel.projectview.generator.sections.ProjectViewJavaPathSectionGenerator
-import org.jetbrains.bsp.bazel.projectview.generator.sections.ProjectViewTargetsSectionGenerator
+import org.jetbrains.bsp.bazel.projectview.generator.sections.*
 import org.jetbrains.bsp.bazel.projectview.model.ProjectView
 import org.jetbrains.bsp.bazel.utils.dope.DopeFiles
 import java.nio.file.Path
@@ -22,5 +18,6 @@ object DefaultProjectViewGenerator : ProjectViewGenerator {
             ProjectViewDebuggerAddressSectionGenerator.generatePrettyString(projectView.debuggerAddress),
             ProjectViewJavaPathSectionGenerator.generatePrettyString(projectView.javaPath),
             ProjectViewBuildFlagsSectionGenerator.generatePrettyString(projectView.buildFlags),
+            ProjectViewBuildManualTargetsSectionGenerator.generatePrettyString(projectView.buildManualTargets),
         ).joinToString(separator = "\n\n", postfix = "\n")
 }
