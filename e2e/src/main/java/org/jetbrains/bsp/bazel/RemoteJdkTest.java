5bcfae4b9a8094513eff63fecb5860eaec724dee
Basak Kapusuzoglu <basak.kapusuzoglu@jetbrains.com>
2022-06-10 14:51:58 +0200

    format

--- e2e/src/main/java/org/jetbrains/bsp/bazel/RemoteJdkTest.java
+++ e2e/src/main/java/org/jetbrains/bsp/bazel/RemoteJdkTest.java
@@ -64,6 +64,7 @@ public class RemoteJdkTest extends BazelBspTestBaseScenario {
 
     return new BazelBspTestScenarioStep(
         "remote-jdk-project workspace build targets",
-        () -> testClient.testWorkspaceTargets(Duration.ofSeconds(300), workspaceBuildTargetsResult));
+        () ->
+            testClient.testWorkspaceTargets(Duration.ofSeconds(300), workspaceBuildTargetsResult));
   }
 }
