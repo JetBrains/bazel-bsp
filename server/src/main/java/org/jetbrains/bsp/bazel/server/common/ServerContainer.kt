2885f22b4aae3360f7489e29dff4e186abfff669
mikkoziel <34162085+mikkoziel@users.noreply.github.com>
2022-07-18 10:37:29 +0200

    [change] added cache of targets info in incremental syncs (#273)
    Co-authored-by: Mikołaj Kozieł <mkoziel@twitter.com>

--- server/src/main/java/org/jetbrains/bsp/bazel/server/common/ServerContainer.kt
+++ server/src/main/java/org/jetbrains/bsp/bazel/server/common/ServerContainer.kt
@@ -11,13 +11,7 @@ import org.jetbrains.bsp.bazel.server.bsp.info.BspInfo
 import org.jetbrains.bsp.bazel.server.bsp.managers.BazelBspAspectsManager
 import org.jetbrains.bsp.bazel.server.bsp.managers.BazelBspCompilationManager
 import org.jetbrains.bsp.bazel.server.bsp.utils.InternalAspectsResolver
-import org.jetbrains.bsp.bazel.server.sync.BazelPathsResolver
-import org.jetbrains.bsp.bazel.server.sync.BazelProjectMapper
-import org.jetbrains.bsp.bazel.server.sync.FileProjectStorage
-import org.jetbrains.bsp.bazel.server.sync.ProjectProvider
-import org.jetbrains.bsp.bazel.server.sync.ProjectResolver
-import org.jetbrains.bsp.bazel.server.sync.ProjectStorage
-import org.jetbrains.bsp.bazel.server.sync.TargetKindResolver
+import org.jetbrains.bsp.bazel.server.sync.*
 import org.jetbrains.bsp.bazel.server.sync.languages.LanguagePluginsService
 import org.jetbrains.bsp.bazel.server.sync.languages.cpp.CppLanguagePlugin
 import org.jetbrains.bsp.bazel.server.sync.languages.java.JavaLanguagePlugin
@@ -72,11 +66,13 @@ class ServerContainer internal constructor(
             val targetKindResolver = TargetKindResolver()
             val bazelProjectMapper =
                 BazelProjectMapper(languagePluginsService, bazelPathsResolver, targetKindResolver)
+            val targetInfoReader = TargetInfoReader()
             val projectResolver = ProjectResolver(
                 bazelBspAspectsManager,
                 workspaceContextProvider,
                 bazelProjectMapper,
-                bspClientLogger
+                bspClientLogger,
+                targetInfoReader
             )
             val finalProjectStorage = projectStorage ?: FileProjectStorage(bspInfo, bspClientLogger)
             val projectProvider = ProjectProvider(projectResolver, finalProjectStorage)
--- server/src/main/java/org/jetbrains/bsp/bazel/server/sync/ProjectResolver.kt
+++ server/src/main/java/org/jetbrains/bsp/bazel/server/sync/ProjectResolver.kt
@@ -1,11 +1,5 @@
 package org.jetbrains.bsp.bazel.server.sync
 
-import com.google.protobuf.TextFormat
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.runBlocking
 import org.jetbrains.bsp.bazel.info.BspTargetInfo.TargetInfo
 import org.jetbrains.bsp.bazel.logger.BspClientLogger
 import org.jetbrains.bsp.bazel.server.bep.BepOutput
@@ -14,16 +8,14 @@ import org.jetbrains.bsp.bazel.server.sync.model.Project
 import org.jetbrains.bsp.bazel.workspacecontext.WorkspaceContext
 import org.jetbrains.bsp.bazel.workspacecontext.WorkspaceContextProvider
 import java.net.URI
-import java.nio.charset.StandardCharsets
-import java.nio.file.Files
-import java.nio.file.Paths
 
 /** Responsible for querying bazel and constructing Project instance  */
 class ProjectResolver(
     private val bazelBspAspectsManager: BazelBspAspectsManager,
     private val workspaceContextProvider: WorkspaceContextProvider,
     private val bazelProjectMapper: BazelProjectMapper,
-    private val logger: BspClientLogger
+    private val logger: BspClientLogger,
+    private val targetInfoReader: TargetInfoReader
 ) {
     fun resolve(): Project {
         val workspaceContext = logger.timed(
@@ -39,7 +31,7 @@ class ProjectResolver(
         val rootTargets = bepOutput.rootTargets()
         val targets = logger.timed<Map<String, TargetInfo>>(
             "Parsing aspect outputs"
-        ) { readTargetMapFromAspectOutputs(aspectOutputs) }
+        ) { targetInfoReader.readTargetMapFromAspectOutputs(aspectOutputs) }
         return logger.timed<Project>(
             "Mapping to internal model"
         ) { bazelProjectMapper.createProject(targets, rootTargets, workspaceContext) }
@@ -52,17 +44,6 @@ class ProjectResolver(
             listOf(BSP_INFO_OUTPUT_GROUP, ARTIFACTS_OUTPUT_GROUP)
         )
 
-    private fun readTargetMapFromAspectOutputs(files: Set<URI>): Map<String, TargetInfo> =
-        runBlocking(Dispatchers.IO) {
-            files.asFlow().map(::readTargetInfoFromFile).toList().associateBy(TargetInfo::getId)
-        }
-
-    private fun readTargetInfoFromFile(uri: URI): TargetInfo {
-        val builder = TargetInfo.newBuilder()
-        val parser = TextFormat.Parser.newBuilder().setAllowUnknownFields(true).build()
-        parser.merge(Files.newBufferedReader(Paths.get(uri), StandardCharsets.UTF_8), builder)
-        return builder.build()
-    }
 
     companion object {
         private const val ASPECT_NAME = "bsp_target_info_aspect"
