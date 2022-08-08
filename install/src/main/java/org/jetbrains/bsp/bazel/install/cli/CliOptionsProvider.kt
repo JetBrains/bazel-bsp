18db5c9c71f045c275d0951f71a2a8fbdaf83030
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2022-08-08 16:51:35 +0200

    help fix

--- install/src/main/java/org/jetbrains/bsp/bazel/install/cli/CliOptionsProvider.kt
+++ install/src/main/java/org/jetbrains/bsp/bazel/install/cli/CliOptionsProvider.kt
@@ -169,7 +169,7 @@ class CliOptionsProvider(private val args: Array<String>) {
         val bazelWorkspaceRootDirOption = Option.builder(BAZEL_WORKSPACE_ROOT_DIR_OPT)
             .longOpt("bazel-workspace")
             .hasArg()
-            .desc("Add path to Bazel project's root directory. By default, it is the same as -$BAZEL_PATH_SHORT_OPT")
+            .desc("Add path to Bazel project's root directory. By default, it is the same as --directory (-d)")
             .build()
         cliParserOptions.addOption(bazelWorkspaceRootDirOption)
     }
