734074741d9e400e6fd2e2155829d67a4f569416
Błażej Kardyś <bkardys@virtuslab.com>
2022-10-31 09:35:56 +0100

    [fix] correctly determine language type for JVM binary targets (#306)

--- server/src/main/java/org/jetbrains/bsp/bazel/server/sync/model/Language.kt
+++ server/src/main/java/org/jetbrains/bsp/bazel/server/sync/model/Language.kt
@@ -3,11 +3,12 @@ package org.jetbrains.bsp.bazel.server.sync.model
 enum class Language(
     val id: String,
     val extensions: Set<String>,
-    dependentNames: Set<String> = hashSetOf()
+    val binary_targets: Set<String> = hashSetOf(),
+    dependentNames: Set<String> = hashSetOf(),
 ) {
     SCALA("scala", hashSetOf(".scala")),
-    JAVA("java", hashSetOf(".java")),
-    KOTLIN("kotlin", hashSetOf(".kt"), hashSetOf(JAVA.id)),
+    JAVA("java", hashSetOf(".java"), binary_targets = setOf("java_binary")),
+    KOTLIN("kotlin", hashSetOf(".kt"), setOf("kt_jvm_binary"), hashSetOf(JAVA.id)),
     CPP("cpp", hashSetOf(".C", ".cc", ".cpp", ".CPP", ".c++", ".cp", "cxx", ".h", ".hpp")),
     THRIFT("thrift", hashSetOf(".thrift"));
 
