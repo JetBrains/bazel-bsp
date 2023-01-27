6050f6af1b3416bf334b641cfd4b95e859133103
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2023-01-27 17:05:38 +0100

    one more fix

--- executioncontext/workspacecontext/src/test/java/org/jetbrains/bsp/bazel/workspacecontext/BazelPathSpecMapperTest.kt
+++ executioncontext/workspacecontext/src/test/java/org/jetbrains/bsp/bazel/workspacecontext/BazelPathSpecMapperTest.kt
@@ -10,29 +10,29 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import kotlin.io.path.Path
 
-
 class BazelPathSpecMapperTest {
 
     @Nested
     @DisplayName("fun map(projectView): Try<BazelPathSpec> tests")
     inner class MapTest {
 
-    // TODO https://youtrack.jetbrains.com/issue/BAZEL-58 + framework to test envs
-    @Disabled("for now we don't have a framework to change classpath, i'll fix it later")
-    @Test
-    fun `should return failure if it isn't possible to deduct bazel path from PATH and bazel path is null`() {
-       // given
-       val projectView = ProjectView.Builder(bazelPath = null).build().get()
+        // TODO https://youtrack.jetbrains.com/issue/BAZEL-58 + framework to test envs
+        @Disabled("for now we don't have a framework to change classpath, i'll fix it later")
+        @Test
+        fun `should return failure if it isn't possible to deduct bazel path from PATH and bazel path is null`() {
+           // given
+           val projectView = ProjectView.Builder(bazelPath = null).build().get()
 
-       // when
-       val bazelPathSpecTry = BazelPathSpecMapper.map(projectView)
+           // when
+           val bazelPathSpecTry = BazelPathSpecMapper.map(projectView)
 
-       // then
-       bazelPathSpecTry.isFailure shouldBe true
-       bazelPathSpecTry.cause::cause shouldBe ProjectViewToExecutionContextEntityMapperException::class
-       bazelPathSpecTry.cause.message shouldBe "Mapping project view into 'bazel path' failed! Could not find bazel on your PATH"
-    }
+           // then
+           bazelPathSpecTry.isFailure shouldBe true
+           bazelPathSpecTry.cause::cause shouldBe ProjectViewToExecutionContextEntityMapperException::class
+           bazelPathSpecTry.cause.message shouldBe "Mapping project view into 'bazel path' failed! Could not find bazel on your PATH"
+        }
 
+        @Disabled("for now we don't have a framework to change classpath, i'll fix it later")
         @Test
         fun `should return success with deducted bazel path from PATH if bazel path is null`() {
             // given
