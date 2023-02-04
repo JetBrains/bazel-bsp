4f732a39ad732b8ba49808b392b75ab7e7500322
Danil.Tereshchenko <danil.tereshchenko@jetbrains.com>
2023-02-04 13:14:56 +0400

    change e2e tests to bazel runner, add jdk export for local JDK test

--- .teamcity/configurations/e2eTests.kts
+++ .teamcity/configurations/e2eTests.kts
@@ -1,5 +1,6 @@
 package configurations
 
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildSteps
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.ScriptBuildStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.BazelStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
@@ -7,30 +8,47 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.bazel
 import jetbrains.buildServer.configs.kotlin.v2019_2.FailureConditions
 
 
-open class BazelBspE2ETestsBuildType(targets: String, failureConditions: FailureConditions.() -> Unit = {}) : BaseConfiguration.BaseBuildType(
+open class BazelBspE2ETestsBuildType(
+    targets: String,
+    failureConditions: FailureConditions.() -> Unit = {},
+    steps: (BuildSteps.() -> Unit)? = null
+) : BaseConfiguration.BaseBuildType(
+
     name = "[e2e tests] $targets test",
     setupSteps = true,
     steps = {
-        script {
+        bazel {
             this.name = "test $targets"
-            scriptContent = """
-                #!/bin/bash
-                set -euxo pipefail
-                
-                export JAVA_HOME=%env.JDK_11_0%
-                
-                bazel run $targets
-            """.trimIndent()
+            this.command = "run"
+            this.targets = targets
+            logging = BazelStep.Verbosity.Diagnostic
+            param("toolPath", "/usr/bin")
         }
     },
     failureConditions = failureConditions,
-)
+
+    )
 
 object SampleRepoE2ETest : BazelBspE2ETestsBuildType(
     targets = "//e2e:BazelBspSampleRepoTest",
 )
+
 object BazelBspLocalJdkTest : BazelBspE2ETestsBuildType(
     targets = "//e2e:BazelBspLocalJdkTest",
+    steps = {
+        script {
+            this.name = "set JDK to 11"
+            scriptContent = """
+                #!/bin/bash
+                set -euxo pipefail
+                
+                export PATH="%env.JDK_11_0%/bin:($)PATH"
+                echo "##teamcity[setParameter name='env.PATH' value='($)PATH']"
+                
+                which java
+            """.trimIndent()
+        }
+    }
 )
 
 object BazelBspRemoteJdkTest : BazelBspE2ETestsBuildType(
