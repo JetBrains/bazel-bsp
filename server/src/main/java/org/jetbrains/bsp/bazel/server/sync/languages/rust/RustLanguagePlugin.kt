2f6783d685e126259759335101d7e74beedb5137
Aleksander Tudruj <aleksanderwt@gmail.com>
2023-04-29 00:17:02 +0200

    PR feedback: refactored rustPackages function

--- server/src/main/java/org/jetbrains/bsp/bazel/server/sync/languages/rust/RustLanguagePlugin.kt
+++ server/src/main/java/org/jetbrains/bsp/bazel/server/sync/languages/rust/RustLanguagePlugin.kt
@@ -19,7 +19,6 @@ import org.jetbrains.bsp.bazel.info.BspTargetInfo.TargetInfo
 import org.jetbrains.bsp.bazel.server.sync.BazelPathsResolver
 import org.jetbrains.bsp.bazel.server.sync.model.Module
 import org.jetbrains.bsp.bazel.server.sync.languages.LanguagePlugin
-import org.jetbrains.bsp.bazel.info.BspTargetInfo.RustCrateLocation as ProtoRustCrateLocation
 import org.jetbrains.bsp.bazel.server.sync.model.Label
 import org.jetbrains.bsp.bazel.server.sync.model.Language
 
@@ -91,10 +90,10 @@ class RustLanguagePlugin(private val bazelPathsResolver: BazelPathsResolver) : L
     }
 
     // We need to merge targets with the same crate root.
-    private fun mergeTargets(rustTargets: List<Pair<Module, RustModule>>): Pair<Module, RustModule> {
+    private fun mergeTargets(rustTargets: List<RustTargetModule>): RustTargetModule {
         require(rustTargets.isNotEmpty()) { "Cannot merge an empty list of targets." }
         if (rustTargets.size == 1) {
-            return rustTargets[0]
+            return rustTargets.first()
         }
 
         // TODO:
@@ -102,106 +101,160 @@ class RustLanguagePlugin(private val bazelPathsResolver: BazelPathsResolver) : L
         //   the most features. It can be changes as a union of features set, but we need
         //   to give it a proper label.
 
-        return rustTargets.maxBy { (_, rustData) -> rustData.crateFeatures.size }
+        return rustTargets.maxByOrNull { (_, rustData) -> rustData.crateFeatures.size }!!
     }
 
     private fun prependWorkspacePath(path: String): String =
         bazelPathsResolver.relativePathToExecRootAbsolute(path).toString()
 
-    private fun rustPackages(rustBspTargets: List<Module>): List<RustPackage> {
-        val packages = rustBspTargets
-            .groupBy { resolvePackage(it).packageName }
-            .mapNotNull { (rustPackage, rustTargets) ->
-                val allRustTargetsWithData: List<Pair<Module, RustModule>> = rustTargets.mapNotNull {
-                    if (it.languageData is RustModule) {
-                        Pair(it, it.languageData)
-                    } else {
-                        null
-                    }
-                }
-                // With removed `duplicates` for the same crate root
-                val rustTargetsWithData: List<Pair<Module, RustModule>> =
-                    allRustTargetsWithData.groupBy { (_, rustData) ->
-                        rustData.crateRoot
-                    }.map { (_, targets) ->
-                        mergeTargets(targets)
-                    }
-
-                // target have versions, but package no, as there is no such thing as package in Rust in Bazel
-                val version = rustTargetsWithData.map { (_, rustData) -> rustData.version }.maxOf { it }
-                val (major, minor, patch) = version.split(".")
-                val isFromWorkspace = rustTargetsWithData.any { (_, rustData) -> rustData.fromWorkspace }
-                val origin = if (isFromWorkspace) {
-                    "WORKSPACE"
-                } else {
-                    "DEP"
-                }
-                val edition = rustTargetsWithData.map { (_, rustData) -> rustData.edition }.maxOf { it }
-                val source = if (isFromWorkspace) {
-                    null
-                } else {
-                    "bazel+https://github.com/rust-lang/crates.io-index"
-                }     // let's hope it is
-                val pkgBaseDir = rustTargetsWithData.first().first.baseDirectory.toString()
-
-                val mapTarget = { (genericData, rustData): Pair<Module, RustModule> ->
-                    RustTarget(
-                        resolvePackage(genericData).targetName,
-                        rustData.crateRoot,
-                        genericData.baseDirectory.toString(),
-                        rustData.kind,
-                        rustData.edition,
-                        false,                                  // TODO: check it somehow. I even know where to look for it :/  http://bazelbuild.github.io/rules_rust/rust_doc.html
-                        rustData.crateFeatures
-                    )
-                }
-
-                val procMacroArtifacts = rustTargetsWithData
-                    .flatMap { (_, rustData) -> rustData.procMacroArtifacts }
-                    .map { bazelPathsResolver.pathToDirectoryUri(it) }
-                    .map { RustProcMacroArtifact(it.path, "") }
-
-                val procMacroArtifact = procMacroArtifacts.firstOrNull() // TODO: oh yeah, we need toolchain here...
-
-                val targets = rustTargetsWithData.map(mapTarget)
-                val allTargets = allRustTargetsWithData.map(mapTarget)
-                val allFeatures = rustTargetsWithData.flatMap { (_, rustData) ->
-                    rustData.crateFeatures
-                }
-                RustPackage(
-                    rustPackage,
-                    version,
-                    origin,
-                    edition,
-                    source,
-                    targets,
-                    allTargets,
-                    allFeatures.map { RustFeature(it, listOf()) },
-                    allFeatures,
-                    null,
-                    listOf(
-                        RustEnvData("CARGO_MANIFEST_DIF", "$pkgBaseDir${rustPackage.drop(1)}"),
-                        RustEnvData("CARGO", "cargo"),
-                        RustEnvData("CARGO_PKG_VERSION", version),
-                        RustEnvData("CARGO_PKG_VERSION_MAJOR", major),
-                        RustEnvData("CARGO_PKG_VERSION_MINOR", minor),
-                        RustEnvData("CARGO_PKG_VERSION_PATCH", patch),
-                        RustEnvData("CARGO_PKG_VERSION_PRE", ""),
-                        RustEnvData("CARGO_PKG_AUTHORS", ""),
-                        RustEnvData("CARGO_PKG_NAME", rustPackage),
-                        RustEnvData("CARGO_PKG_DESCRIPTION", ""),
-                        RustEnvData("CARGO_PKG_REPOSITORY", ""),
-                        RustEnvData("CARGO_PKG_LICENSE", ""),
-                        RustEnvData("CARGO_PKG_LICENSE_FILE", ""),
-                        RustEnvData("CARGO_CRATE_NAME", rustPackage),
-                    ),
-                    null,
-                    procMacroArtifact
-                )
+    private fun serveTargetWithRustData(rustTargets: List<Module>): List<RustTargetModule> =
+        rustTargets.mapNotNull {
+            if (it.languageData is RustModule) {
+                Pair(it, it.languageData)
+            } else {
+                null
             }
+        }
 
-        return packages
+    private fun removeConflictingRustTargets(allRustTargets: List<RustTargetModule>): List<RustTargetModule> =
+        allRustTargets.groupBy { (_, rustData) ->
+            rustData.crateRoot
+        }.map { (_, targets) ->
+            mergeTargets(targets)
+        }
+
+    private fun splitVersion(version: String): Triple<String, String, String> = try {
+        val (major, minor, patch) = version.split(".")
+        Triple(major, minor, patch)
+    } catch (_: Exception) {
+        Triple(version, "", "")
     }
+
+    private fun resolvePackageVersion(rustTargets: List<RustTargetModule>): String =
+        rustTargets.map { (_, rustData) -> rustData.version }.maxOf { it }
+
+    private fun isPackageFromWorkspace(rustTargets: List<RustTargetModule>): Boolean {
+        val isFromWorkspace = rustTargets.any { (_, rustData) -> rustData.fromWorkspace }
+        require(rustTargets.all { (_, rustData) -> rustData.fromWorkspace == isFromWorkspace }) {
+            "All targets within a single package must have the same origin."
+        }
+        return isFromWorkspace
+    }
+
+    private fun resolvePackageOrigin(isFromWorkspace: Boolean): String = if (isFromWorkspace) {
+        "WORKSPACE"
+    } else {
+        "DEP"
+    }
+
+    private fun resolvePackageEdition(rustTargets: List<RustTargetModule>): String =
+        rustTargets.map { (_, rustData) -> rustData.edition }.maxOf { it }
+
+    private fun resolvePackageSource(isFromWorkspace: Boolean): String? = if (isFromWorkspace) {
+        null
+    } else {
+        // let's hope it is
+        "bazel+https://github.com/rust-lang/crates.io-index"
+    }
+
+    private fun resolvePackageBaseDir(rustTargets: List<RustTargetModule>): String {
+        val baseDirs = rustTargets.map { (pkg, _) -> pkg.baseDirectory.toString() }.distinct()
+        require(baseDirs.size == 1) { "All targets within a single package must have the same base directory." }
+        return baseDirs.first()
+    }
+
+    private fun parseSingleTarget(module: RustTargetModule): RustTarget {
+        val (genericData, rustData) = module
+        return RustTarget(
+            resolvePackage(genericData).targetName,
+            rustData.crateRoot,
+            genericData.baseDirectory.toString(),
+            rustData.kind,
+            rustData.edition,
+            false,                  // TODO: check it somehow. I even know where to look for it :/  http://bazelbuild.github.io/rules_rust/rust_doc.html
+            rustData.crateFeatures
+        )
+    }
+
+    private fun resolveProcMacroArtifact(rustTargets: List<RustTargetModule>): RustProcMacroArtifact? =
+        rustTargets
+            .flatMap { (_, rustData) -> rustData.procMacroArtifacts }
+            .map { bazelPathsResolver.pathToDirectoryUri(it) }
+            .map { RustProcMacroArtifact(it.path, "") }
+            .firstOrNull()
+    // TODO: ^^^^^ Cargo does not allow for more then one library in a single package so no more than one proc macro.
+
+    private fun resolvePackageFeatures(
+        rustTargets: List<RustTargetModule>
+    ): Pair<List<String>, List<RustFeature>> {
+        val allFeaturesAsStrings = rustTargets.flatMap { (_, rustData) ->
+            rustData.crateFeatures
+        }
+        val allFeatures = allFeaturesAsStrings.map { RustFeature(it, listOf()) }
+        return Pair(allFeaturesAsStrings, allFeatures)
+    }
+
+    private fun resolvePackageEnv(rustPackage: String, pkgBaseDir: String, version: String): List<RustEnvData> {
+        val (major, minor, patch) = splitVersion(version)
+        val envMap = mapOf(
+            "CARGO_MANIFEST_DIR" to "$pkgBaseDir${rustPackage.drop(1)}",
+            "CARGO" to "cargo",
+            "CARGO_PKG_VERSION" to version,
+            "CARGO_PKG_VERSION_MAJOR" to major,
+            "CARGO_PKG_VERSION_MINOR" to minor,
+            "CARGO_PKG_VERSION_PATCH" to patch,
+            "CARGO_PKG_VERSION_PRE" to "",
+            "CARGO_PKG_AUTHORS" to "",
+            "CARGO_PKG_NAME" to rustPackage,
+            "CARGO_PKG_DESCRIPTION" to "",
+            "CARGO_PKG_REPOSITORY" to "",
+            "CARGO_PKG_LICENSE" to "",
+            "CARGO_PKG_LICENSE_FILE" to "",
+            "CARGO_CRATE_NAME" to rustPackage,
+        )
+        return envMap.map { RustEnvData(it.key, it.value) }
+    }
+
+    private fun resolveSinglePackage(packageData: Map.Entry<String, List<Module>>): RustPackage {
+        val (rustPackage, rustTargets) = packageData
+        val allRustTargetsWithData = serveTargetWithRustData(rustTargets)
+        // With removed `duplicates` for the same crate root
+        val rustTargetsWithData = removeConflictingRustTargets(allRustTargetsWithData)
+
+        // target have versions, but package no, as there is no such thing as package in Rust in Bazel
+        val version = resolvePackageVersion(rustTargetsWithData)
+        val isFromWorkspace = isPackageFromWorkspace(rustTargetsWithData)
+        val origin = resolvePackageOrigin(isFromWorkspace)
+        val edition = resolvePackageEdition(rustTargetsWithData)
+        val source = resolvePackageSource(isFromWorkspace)
+        val pkgBaseDir = resolvePackageBaseDir(rustTargetsWithData)
+        val procMacroArtifact = resolveProcMacroArtifact(rustTargetsWithData)
+
+        val targets = rustTargetsWithData.map(::parseSingleTarget)
+        val allTargets = allRustTargetsWithData.map(::parseSingleTarget)
+        val (allFeaturesAsString, allFeatures) = resolvePackageFeatures(rustTargetsWithData)
+        val env = resolvePackageEnv(rustPackage, pkgBaseDir, version)
+
+        return RustPackage(
+            rustPackage,
+            version,
+            origin,
+            edition,
+            source,
+            targets,
+            allTargets,
+            allFeatures,
+            allFeaturesAsString,
+            null,
+            env,
+            null,
+            procMacroArtifact
+        )
+    }
+
+    private fun rustPackages(rustBspTargets: List<Module>): List<RustPackage> = rustBspTargets
+        .groupBy { resolvePackage(it).packageName }
+        .mapNotNull(::resolveSinglePackage)
 
     // We need to resolve all dependencies are provide a list of new bazel targets (deps) to be transformed into packages.
     private fun rustDependencies(
@@ -337,4 +390,6 @@ class RustLanguagePlugin(private val bazelPathsResolver: BazelPathsResolver) : L
     }
 }
 
-private data class BazelPackageTargetInfo(val packageName: String, val targetName: String)
+data class BazelPackageTargetInfo(val packageName: String, val targetName: String)
+// TODO: add private
+typealias RustTargetModule = Pair<Module, RustModule>
