e6f4db8492389bd4a4a7bcc3af3ab86f19e4fb59
Tomasz Pasternak <1066003+tpasternak@users.noreply.github.com>
2023-06-14 02:11:35 -0700

    [performance] Large synthetic project benchmark  (#429)
    * [performance] Large synthetic project benchmark
    
    Prepare benchmark
    
    Co-authored-by: Marcin Kocot <marcin.kocot@jetbrains.com>
    
    * Apply review comments
    
    ---------
    
    Co-authored-by: Marcin Kocot <marcin.kocot@jetbrains.com>

--- bspcli/src/java/org/jetbrains/bsp/cli/Main.kt
+++ bspcli/src/java/org/jetbrains/bsp/cli/Main.kt
@@ -2,6 +2,13 @@ package org.jetbrains.bsp.cli
 
 import ch.epfl.scala.bsp4j.*
 import com.google.common.util.concurrent.ThreadFactoryBuilder
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.core.Layout
+import org.apache.logging.log4j.core.LoggerContext
+import org.apache.logging.log4j.core.appender.ConsoleAppender
+import org.apache.logging.log4j.core.layout.PatternLayout
+
+
 import org.eclipse.lsp4j.jsonrpc.Launcher
 import org.eclipse.lsp4j.jsonrpc.Launcher.Builder
 import org.jetbrains.bsp.bazel.install.Install
@@ -12,25 +19,30 @@ import org.jetbrains.bsp.bazel.workspacecontext.DefaultWorkspaceContextProvider
 import java.io.PipedInputStream
 import java.io.PipedOutputStream
 import java.io.PrintStream
+import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.ThreadFactory
+
+
 import kotlin.system.exitProcess
 
 /**
  * The application expects just a single argument - path to your bazel project
  */
 fun main(args: Array<String>) {
-    if(args.size != 1) {
+    if (args.size != 1) {
         println("Invalid number of arguments. Just pass path to your workspace as a CLI argument to this app")
         System.exit(1)
     }
     val workspace = Paths.get(args[0])
+    val installationDirectory = Files.createTempDirectory("bazelbsp-dir-")
     Install.main(arrayOf(
             "--bazel-workspace", workspace.toString(),
-            "--directory", workspace.toString(),
+            "--directory", installationDirectory.toString(),
+            "--targets", "//..."
     ))
 
     val serverOut = PipedInputStream()
@@ -39,7 +51,7 @@ fun main(args: Array<String>) {
     val clientIn = PrintStream(PipedOutputStream(clientOut), true)
 
     val serverExecutor = Executors.newFixedThreadPool(4, threadFactory("cli-server-pool-%d"))
-    val serverLauncher = startServer(serverIn, clientOut, serverExecutor, workspace)
+    val serverLauncher = startServer(serverIn, clientOut, serverExecutor, workspace, installationDirectory)
     val serverAlveFuture = serverLauncher.startListening()
 
 
@@ -88,44 +100,30 @@ private fun startClient(serverOut: PipedInputStream, clientIn: PrintStream, clie
 private fun startServer(serverIn: PrintStream,
                         clientOut: PipedInputStream,
                         serverExecutor: ExecutorService,
-                        workspace: Path): Launcher<ch.epfl.scala.bsp4j.BuildClient> {
-    val bspInfo = BspInfo(workspace)
+                        workspace: Path,
+                        directory: Path): Launcher<ch.epfl.scala.bsp4j.BuildClient> {
+    val bspInfo = BspInfo(directory)
     val bspIntegrationData = BspIntegrationData(serverIn, clientOut, serverExecutor, null)
-    val workspaceContextProvider = DefaultWorkspaceContextProvider(workspace.resolve("projectview.bazelproject"))
+    val workspaceContextProvider = DefaultWorkspaceContextProvider(directory.resolve("projectview.bazelproject"))
     val bspServer = BazelBspServer(bspInfo, workspaceContextProvider, workspace)
     bspServer.startServer(bspIntegrationData)
     return bspIntegrationData.launcher
 }
 
 
-
 class BuildClient : ch.epfl.scala.bsp4j.BuildClient {
-    override fun onBuildShowMessage(params: ShowMessageParams?) {
-        TODO("Not yet implemented")
-    }
+    override fun onBuildShowMessage(params: ShowMessageParams?) {}
 
-    override fun onBuildLogMessage(params: LogMessageParams?) {
-        println(params)
-    }
+    override fun onBuildLogMessage(params: LogMessageParams?) {}
 
-    override fun onBuildTaskStart(params: TaskStartParams?) {
-        TODO("Not yet implemented")
-    }
+    override fun onBuildTaskStart(params: TaskStartParams?) {}
 
-    override fun onBuildTaskProgress(params: TaskProgressParams?) {
-        TODO("Not yet implemented")
-    }
+    override fun onBuildTaskProgress(params: TaskProgressParams?) {}
 
-    override fun onBuildTaskFinish(params: TaskFinishParams?) {
-        TODO("Not yet implemented")
-    }
+    override fun onBuildTaskFinish(params: TaskFinishParams?) {}
 
-    override fun onBuildPublishDiagnostics(params: PublishDiagnosticsParams?) {
-        TODO("Not yet implemented")
-    }
+    override fun onBuildPublishDiagnostics(params: PublishDiagnosticsParams?) {}
 
-    override fun onBuildTargetDidChange(params: DidChangeBuildTarget?) {
-        TODO("Not yet implemented")
-    }
+    override fun onBuildTargetDidChange(params: DidChangeBuildTarget?) {}
 
 }
