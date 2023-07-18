3605dbb20fa8b0309af0e65805cf1698afdec841
Tomasz Pasternak <tomasz.pasternak@jetbrains.com>
2023-07-18 12:22:22 +0000

    [maintenance] Configurable targets size
    Merge-request: BAZEL-MR-373
    Merged-by: Tomasz Pasternak <Tomasz.Pasternak@jetbrains.com>

--- bspcli/gen.py
+++ bspcli/gen.py
@@ -1,4 +1,5 @@
 import os
+import parser
 import sys
 from random import choice
 import random
@@ -10,20 +11,21 @@ def randname():
     )
 
 
-def create_package(workspace_dir, packagename, deps):
+def create_java_package(workspace_dir, packagename, deps, number_of_sources):
     packagedir = f'{workspace_dir}/{packagename}'
     os.mkdir(packagedir)
-    classname = "Lib_" + packagename
     with open(f'{packagedir}/BUILD', 'w') as f:
         f.write(f"""java_library(
           name = "{packagename}",
           visibility = ["//visibility:public"],
-          srcs = ["{classname}.java"],
+          srcs = glob(["*.java"]),
           deps = {deps}
         )\n""")
-    with open(f'{packagedir}/{classname}.java', 'w') as f:
-        f.write(f"""class {classname} {"{}"}\n""")
-        pass
+    for i in range(number_of_sources):
+        classname = f"Lib_{packagename}_{i}"
+        with open(f'{packagedir}/{classname}.java', 'w') as f:
+            f.write(f"""public class {classname} {"{}"}\n""")
+            pass
 
 
 def build_dependency_graph(targets):
@@ -42,8 +44,11 @@ def build_dependency_graph(targets):
 CHARSET = 'abcdefghijklmnopqrstuvwxyz'
 NAMELEN: int = 8
 random.seed(0)
-project_dir = sys.argv[1]
-number_of_targets = int(sys.argv[2])
+
+args = parser.parser().parse_args()
+
+project_dir = args.projectdir
+number_of_targets = args.targets
 targets = [randname() for x in range(number_of_targets)]
 
 if not os.path.exists(f'./{project_dir}'):
@@ -54,7 +59,7 @@ else:
 dependency_graph = build_dependency_graph(targets)
 
 for target, deps in dependency_graph.items():
-    create_package(f'{project_dir}/', target, [f'//{d}:{d}' for d in deps])
+    create_java_package(f'{project_dir}/', target, [f'//{d}:{d}' for d in deps], args.targetssize)
 
 with open(f'{project_dir}/WORKSPACE', 'w') as fp:
     pass
