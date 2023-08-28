0f6b43264a614bcc2af8cec1d9d021a3595474ca
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2023-08-28 13:00:39 +0000

    [maintenance] e2e tests now use bazel path to determine target prefix and local jdk tests are green
    or maybe both will work
    
    now should be fine
    
    [maintenance] e2e tests now use bazel path to determine target prefix
    
    
    Merge-request: BAZEL-MR-477
    Merged-by: Marcin Abramowicz <marcin.abramowicz@jetbrains.com>

--- e2e/src/main/java/org/jetbrains/bsp/bazel/base/BazelBspTestBaseScenario.kt
+++ e2e/src/main/java/org/jetbrains/bsp/bazel/base/BazelBspTestBaseScenario.kt
@@ -6,6 +6,8 @@ import org.apache.logging.log4j.LogManager
 import org.jetbrains.bsp.bazel.install.Install
 import org.jetbrains.bsp.testkit.client.bazel.BazelTestClient
 import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.name
 import kotlin.system.exitProcess
 
 abstract class BazelBspTestBaseScenario {
@@ -13,15 +15,19 @@ abstract class BazelBspTestBaseScenario {
     private val binary = System.getenv("BIT_BAZEL_BINARY")
     private val workspaceDir = System.getenv("BIT_WORKSPACE_DIR")
 
-    val targetPrefix = Runtime.getRuntime().exec("$binary version").inputStream.bufferedReader().readText()
-            .let {"""(?<=Build label: )\d+(?=[0-9.]+)""".toRegex().find(it)!!.value.toInt() }
-            .let { if(it < 6) "" else "@" }
-
+    val targetPrefix = calculateTargetPrefix()
     protected val testClient: BazelTestClient
 
     init {
         installServer()
         testClient = createClient()
+    }
+
+    // check: https://github.com/bazelbuild/intellij/blob/adb358670a7fc6ad51808486dc03f4605f83dcd3/aspect/testing/tests/src/com/google/idea/blaze/aspect/integration/BazelInvokingIntegrationTestRunner.java#L132
+    private fun calculateTargetPrefix(): String {
+        val dirName = Path(binary).parent.name
+        val majorVersion = dirName.split("_")[3].toInt()
+        return if (majorVersion < 6) "" else "@"
     }
 
     private fun installServer() {
