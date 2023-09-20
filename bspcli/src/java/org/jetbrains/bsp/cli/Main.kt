08271309e420136582e409122b3c13b0f5e0eb08
Tomasz Pasternak <tomasz.pasternak@jetbrains.com>
2023-09-20 16:40:06 +0000

    [fix] Race condition in bspcli
    Merge-request: BAZEL-MR-525
    Merged-by: Tomasz Pasternak <Tomasz.Pasternak@jetbrains.com>

--- bspcli/src/java/org/jetbrains/bsp/cli/Main.kt
+++ bspcli/src/java/org/jetbrains/bsp/cli/Main.kt
@@ -1,17 +1,31 @@
 package org.jetbrains.bsp.cli
 
-import ch.epfl.scala.bsp4j.*
-import com.google.common.util.concurrent.ThreadFactoryBuilder
 
+import ch.epfl.scala.bsp4j.BuildClientCapabilities
+import ch.epfl.scala.bsp4j.BuildServer
+import ch.epfl.scala.bsp4j.CppBuildServer
+import ch.epfl.scala.bsp4j.DidChangeBuildTarget
+import ch.epfl.scala.bsp4j.InitializeBuildParams
+import ch.epfl.scala.bsp4j.JavaBuildServer
+import ch.epfl.scala.bsp4j.JvmBuildServer
+import ch.epfl.scala.bsp4j.LogMessageParams
+import ch.epfl.scala.bsp4j.PublishDiagnosticsParams
+import ch.epfl.scala.bsp4j.ScalaBuildServer
+import ch.epfl.scala.bsp4j.ShowMessageParams
+import ch.epfl.scala.bsp4j.TaskFinishParams
+import ch.epfl.scala.bsp4j.TaskProgressParams
+import ch.epfl.scala.bsp4j.TaskStartParams
+import com.google.common.util.concurrent.ThreadFactoryBuilder
 import org.eclipse.lsp4j.jsonrpc.Launcher
 import org.eclipse.lsp4j.jsonrpc.Launcher.Builder
 import org.jetbrains.bsp.bazel.install.Install
 import org.jetbrains.bsp.bazel.server.BazelBspServer
-import org.jetbrains.bsp.bazel.server.sync.BazelBuildServer
-import org.jetbrains.bsp.bazel.server.sync.MetricsLogger
 import org.jetbrains.bsp.bazel.server.bsp.BspIntegrationData
 import org.jetbrains.bsp.bazel.server.bsp.info.BspInfo
+import org.jetbrains.bsp.bazel.server.sync.BazelBuildServer
+import org.jetbrains.bsp.bazel.server.sync.MetricsLogger
 import org.jetbrains.bsp.bazel.workspacecontext.DefaultWorkspaceContextProvider
+import java.io.OutputStream
 import java.io.PipedInputStream
 import java.io.PipedOutputStream
 import java.io.PrintStream
@@ -20,13 +34,14 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.attribute.PosixFilePermission
 import java.nio.file.attribute.PosixFilePermissions
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.ThreadFactory
+import java.util.concurrent.TimeUnit
 import kotlin.io.path.absolute
 import kotlin.io.path.writeText
-
-
 import kotlin.system.exitProcess
 
 interface Api: BuildServer, JvmBuildServer, ScalaBuildServer, JavaBuildServer, CppBuildServer, BazelBuildServer
@@ -69,19 +84,16 @@ fun main(args0: Array<String>) {
             "--targets", args.target
     ))
 
-    val serverOut = PipedInputStream()
-    val serverIn = PrintStream(PipedOutputStream(serverOut), true)
-    val clientOut = PipedInputStream()
-    val clientIn = PrintStream(PipedOutputStream(clientOut), true)
+    val serverOut = FixedThreadPipedOutputStream()
+    val clientOut = FixedThreadPipedOutputStream()
 
     val metricsLogger = MetricsLogger(true)
     val serverExecutor = Executors.newFixedThreadPool(4, threadFactory("cli-server-pool-%d"))
-    val serverLauncher = startServer(serverIn, clientOut, serverExecutor, args.workspace, installationDirectory, metricsLogger)
+    val serverLauncher = startServer(serverOut, clientOut.inputStream, serverExecutor, args.workspace, installationDirectory, metricsLogger)
     val serverAlveFuture = serverLauncher.startListening()
 
-
     val clientExecutor = Executors.newFixedThreadPool(4, threadFactory("cli-client-pool-%d"))
-    val clientLauncher = startClient(serverOut, clientIn, clientExecutor)
+    val clientLauncher = startClient(serverOut.inputStream, clientOut, clientExecutor)
     val clientAliveFuture = clientLauncher.startListening()
 
     val proxy = clientLauncher.remoteProxy
@@ -98,8 +110,8 @@ fun main(args0: Array<String>) {
     clientExecutor.shutdown()
     serverExecutor.shutdown()
 
-    clientIn.close()
-    serverIn.close()
+    clientOut.stop()
+    serverOut.stop()
 
     clientAliveFuture.get()
     serverAlveFuture.get()
@@ -107,6 +119,39 @@ fun main(args0: Array<String>) {
     args.metricsFile?.let {
         it.writeText(metricsLogger.dump())
         println("Metrics dumped to '${it.absolute()}'")
+    }
+}
+
+/**
+ * This class is required, because of limitations of java's PipedStreams.
+ * Unfortunately, whenever PipedInputStream calls read ([code](https://github.com/openjdk/jdk/blob/e30e3564420c631f08ac3d613ab91c93227a00b3/src/java.base/share/classes/java/io/PipedInputStream.java#L314-L316)),
+ * it checks whether the writing thread is alive. Unfortunately, in case of Bazel BSP server, there are a lot of writes
+ * from different threads, that are often spawned only temporarily, from java's Executors.
+ *
+ * The idea how to solve it is to create a single thread, which lifetime is longer than both PipedOutputStream and
+ * PipedInputStream, and it's the only thread that is allowed to write to PipedOutputStream
+ */
+class FixedThreadPipedOutputStream : OutputStream() {
+    val inputStream = PipedInputStream()
+    private val outputStream = PrintStream(PipedOutputStream(inputStream), true)
+    private val queue = ArrayBlockingQueue<Int>(10000)
+    private val _stop = CompletableFuture<Boolean>()
+    private val thread = Thread {
+        while (!_stop.isDone) {
+            queue.poll(100, TimeUnit.MILLISECONDS)
+                    ?.let { outputStream.write(it) }
+        }
+    }.also { it.start() }
+
+    fun stop() {
+        outputStream.close()
+        inputStream.close()
+        _stop.complete(true)
+        thread.join()
+    }
+
+    override fun write(b: Int) {
+        queue.put(b)
     }
 }
 
@@ -119,7 +164,7 @@ private fun threadFactory(nameFormat: String): ThreadFactory =
                 }
                 .build()
 
-private fun startClient(serverOut: PipedInputStream, clientIn: PrintStream, clientExecutor: ExecutorService?): Launcher<Api> =
+private fun startClient(serverOut: PipedInputStream, clientIn: OutputStream, clientExecutor: ExecutorService?): Launcher<Api> =
         Builder<Api>()
                 .setInput(serverOut)
                 .setOutput(clientIn)
@@ -128,7 +173,7 @@ private fun startClient(serverOut: PipedInputStream, clientIn: PrintStream, clie
                 .setLocalService(BuildClient())
                 .create()
 
-private fun startServer(serverIn: PrintStream,
+private fun startServer(serverIn: OutputStream,
                         clientOut: PipedInputStream,
                         serverExecutor: ExecutorService,
                         workspace: Path,
--- server/src/main/java/org/jetbrains/bsp/bazel/server/bsp/BspIntegrationData.kt
+++ server/src/main/java/org/jetbrains/bsp/bazel/server/bsp/BspIntegrationData.kt
@@ -4,12 +4,13 @@ import ch.epfl.scala.bsp4j.BuildClient
 import io.grpc.Server
 import org.eclipse.lsp4j.jsonrpc.Launcher
 import java.io.InputStream
+import java.io.OutputStream
 import java.io.PrintStream
 import java.io.PrintWriter
 import java.util.concurrent.ExecutorService
 
 data class BspIntegrationData(
-    val stdout: PrintStream,
+    val stdout: OutputStream,
     val stdin: InputStream,
     val executor: ExecutorService,
     val traceWriter: PrintWriter?,
