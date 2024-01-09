4e218def53f6d318ca1b1bd7011fe12a9aa162bd
zortenburger <zo438646@students.mimuw.edu.pl>
2024-01-09 23:59:31 +0100

    [feature] create go language plugin | #BAZEL-787

--- server/src/main/kotlin/org/jetbrains/bsp/bazel/server/sync/languages/go/GoLanguagePlugin.kt
+++ server/src/main/kotlin/org/jetbrains/bsp/bazel/server/sync/languages/go/GoLanguagePlugin.kt
@@ -1,6 +1,8 @@
 package org.jetbrains.bsp.bazel.server.sync.languages.go
 
 import ch.epfl.scala.bsp4j.BuildTarget
+import org.jetbrains.bsp.GoBuildTarget
+import org.jetbrains.bsp.bazel.info.BspTargetInfo
 import org.jetbrains.bsp.bazel.server.sync.BazelPathsResolver
 import org.jetbrains.bsp.bazel.server.sync.languages.LanguagePlugin
 
@@ -17,5 +19,15 @@ class GoLanguagePlugin(
 
     buildTarget.dataKind = "go"
     buildTarget.data = goBuildTarget
+  }
+
+  override fun resolveModule(targetInfo: BspTargetInfo.TargetInfo): GoModule? {
+    if (!targetInfo.hasGoTargetInfo()) return null
+
+    val goTargetInfo = targetInfo.goTargetInfo
+    return GoModule(
+      sdkHomePath = goTargetInfo.sdkHomePath,
+      importPath = goTargetInfo.importpath,
+    )
   }
 }
