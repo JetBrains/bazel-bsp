e91a30556cf8955c292d9b29c3efa3ac0d383f7d
Tomasz Godzik <tgodzik@virtuslab.com>
2024-03-26 16:35:48 +0100

    bugfix: Fix publishing warnings
    Previously, no (Scala) warnings would be reported since they would come inside stderr in a progress build event. Now, they are handled via that event and cleared correctly when needed.

--- e2e/src/main/kotlin/org/jetbrains/bsp/bazel/BazelBspScalaProjectTest.kt
+++ e2e/src/main/kotlin/org/jetbrains/bsp/bazel/BazelBspScalaProjectTest.kt
@@ -1,14 +1,9 @@
 package org.jetbrains.bsp.bazel
 
-import ch.epfl.scala.bsp4j.BuildTarget
-import ch.epfl.scala.bsp4j.BuildTargetCapabilities
-import ch.epfl.scala.bsp4j.BuildTargetIdentifier
-import ch.epfl.scala.bsp4j.ScalacOptionsItem
-import ch.epfl.scala.bsp4j.ScalacOptionsParams
-import ch.epfl.scala.bsp4j.ScalacOptionsResult
-import ch.epfl.scala.bsp4j.WorkspaceBuildTargetsResult
+import ch.epfl.scala.bsp4j.*
 import org.jetbrains.bsp.bazel.base.BazelBspTestBaseScenario
 import org.jetbrains.bsp.bazel.base.BazelBspTestScenarioStep
+import java.util.UUID
 import kotlin.time.Duration.Companion.seconds
 
 object BazelBspScalaProjectTest : BazelBspTestBaseScenario() {
@@ -19,43 +14,48 @@ object BazelBspScalaProjectTest : BazelBspTestBaseScenario() {
 
   override fun scenarioSteps(): List<BazelBspTestScenarioStep> = listOf(
     scalaOptionsResults(),
+    compileWithWarnings(),
   )
 
   override fun expectedWorkspaceBuildTargetsResult(): WorkspaceBuildTargetsResult {
-    return WorkspaceBuildTargetsResult(
-      listOf(
-        BuildTarget(
-          BuildTargetIdentifier("@//scala_targets:library"),
-          listOf("library"),
-          listOf("scala"),
-          emptyList(),
-          BuildTargetCapabilities()
-        )
-      )
-    )
+    return WorkspaceBuildTargetsResult(listOf(BuildTarget(BuildTargetIdentifier("@//scala_targets:library"), listOf("library"), listOf("scala"), emptyList(), BuildTargetCapabilities())))
   }
 
 
   private fun scalaOptionsResults(): BazelBspTestScenarioStep {
     val expectedTargetIdentifiers = expectedTargetIdentifiers().filter { it.uri != "bsp-workspace-root" }
     val expectedScalaOptionsItems = expectedTargetIdentifiers.map {
-      ScalacOptionsItem(
-        it,
-        emptyList(),
-        listOf(
-          "file://\$BAZEL_OUTPUT_BASE_PATH/execroot/__main__/bazel-out/k8-fastbuild/bin/external/io_bazel_rules_scala_scala_library/io_bazel_rules_scala_scala_library.stamp/scala-library-2.13.6-stamped.jar",
-          "file://\$BAZEL_OUTPUT_BASE_PATH/execroot/__main__/bazel-out/k8-fastbuild/bin/external/io_bazel_rules_scala_scala_reflect/io_bazel_rules_scala_scala_reflect.stamp/scala-reflect-2.13.6-stamped.jar"
-        ),
-        "file://\$BAZEL_OUTPUT_BASE_PATH/execroot/__main__/bazel-out/k8-fastbuild/bin/scala_targets/library.jar"
-      )
+      ScalacOptionsItem(it, emptyList(), listOf("file://\$BAZEL_OUTPUT_BASE_PATH/execroot/__main__/bazel-out/k8-fastbuild/bin/external/io_bazel_rules_scala_scala_library/io_bazel_rules_scala_scala_library.stamp/scala-library-2.13.6-stamped.jar", "file://\$BAZEL_OUTPUT_BASE_PATH/execroot/__main__/bazel-out/k8-fastbuild/bin/external/io_bazel_rules_scala_scala_reflect/io_bazel_rules_scala_scala_reflect.stamp/scala-reflect-2.13.6-stamped.jar"), "file://\$BAZEL_OUTPUT_BASE_PATH/execroot/__main__/bazel-out/k8-fastbuild/bin/scala_targets/library.jar")
     }
     val expectedScalaOptionsResult = ScalacOptionsResult(expectedScalaOptionsItems)
     val scalaOptionsParams = ScalacOptionsParams(expectedTargetIdentifiers)
-
-    return BazelBspTestScenarioStep(
-      "scalaOptions results"
-    ) {
+    return BazelBspTestScenarioStep("scalaOptions results") {
       testClient.testScalacOptions(60.seconds, scalaOptionsParams, expectedScalaOptionsResult)
+    }
+  }
+
+  private fun compileWithWarnings(): BazelBspTestScenarioStep {
+    val expectedTargetIdentifiers = expectedTargetIdentifiers().filter { it.uri != "bsp-workspace-root" }
+    val compileParams = CompileParams(expectedTargetIdentifiers);
+    compileParams.originId = UUID.randomUUID().toString()
+
+    val expectedCompilerResult = CompileResult(StatusCode.OK)
+    val expectedDiagnostic = Diagnostic(
+      Range(Position(2, 4), Position(2, 4)),
+      "\"match may not be exhaustive.\\nIt would fail on the following input: C(_)\\n  aa match {\\n  ^\""
+    )
+    expectedDiagnostic.severity = DiagnosticSeverity.WARNING
+    val expectedDocumentId = TextDocumentIdentifier("file://\$WORKSPACE/scala_targets/Example.scala")
+    val expectedDiagnosticsParam = PublishDiagnosticsParams(
+      expectedDocumentId,
+      expectedTargetIdentifiers[0],
+      listOf(expectedDiagnostic),
+      true,
+    )
+    expectedDiagnosticsParam.originId = compileParams.originId
+
+    return BazelBspTestScenarioStep("compile results") {
+      testClient.testCompile(60.seconds, compileParams, expectedCompilerResult, listOf(expectedDiagnosticsParam))
     }
   }
 
--- e2e/test-resources/enabled-rules-project/scala_targets/Example.scala
+++ e2e/test-resources/enabled-rules-project/scala_targets/Example.scala
@@ -1,3 +1,12 @@
 package example
 
-object Example extends App{}
+object Example extends App{
+  val aa: A = ???
+  aa match {
+    case B(_) =>
+  }
+}
+
+sealed trait A
+case class B(b: Int) extends A
+case class C(c: Int) extends A
