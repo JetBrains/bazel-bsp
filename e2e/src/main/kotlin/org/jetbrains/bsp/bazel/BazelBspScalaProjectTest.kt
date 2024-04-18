c1b04af474edbe99cf72daa535893d2db1a345f4
tgodzik <tgodzik@virtuslab.com>
2024-04-18 11:25:28 +0200

    chore: Add tests for compilation

--- e2e/src/main/kotlin/org/jetbrains/bsp/bazel/BazelBspScalaProjectTest.kt
+++ e2e/src/main/kotlin/org/jetbrains/bsp/bazel/BazelBspScalaProjectTest.kt
@@ -18,14 +18,32 @@ object BazelBspScalaProjectTest : BazelBspTestBaseScenario() {
   )
 
   override fun expectedWorkspaceBuildTargetsResult(): WorkspaceBuildTargetsResult {
-    return WorkspaceBuildTargetsResult(listOf(BuildTarget(BuildTargetIdentifier("@//scala_targets:library"), listOf("library"), listOf("scala"), emptyList(), BuildTargetCapabilities())))
+    return WorkspaceBuildTargetsResult(
+      listOf(
+        BuildTarget(
+          BuildTargetIdentifier("@//scala_targets:library"),
+          listOf("library"),
+          listOf("scala"),
+          emptyList(),
+          BuildTargetCapabilities()
+        )
+      )
+    )
   }
 
 
   private fun scalaOptionsResults(): BazelBspTestScenarioStep {
     val expectedTargetIdentifiers = expectedTargetIdentifiers().filter { it.uri != "bsp-workspace-root" }
     val expectedScalaOptionsItems = expectedTargetIdentifiers.map {
-      ScalacOptionsItem(it, emptyList(), listOf("file://\$BAZEL_OUTPUT_BASE_PATH/execroot/__main__/bazel-out/k8-fastbuild/bin/external/io_bazel_rules_scala_scala_library/io_bazel_rules_scala_scala_library.stamp/scala-library-2.13.6-stamped.jar", "file://\$BAZEL_OUTPUT_BASE_PATH/execroot/__main__/bazel-out/k8-fastbuild/bin/external/io_bazel_rules_scala_scala_reflect/io_bazel_rules_scala_scala_reflect.stamp/scala-reflect-2.13.6-stamped.jar"), "file://\$BAZEL_OUTPUT_BASE_PATH/execroot/__main__/bazel-out/k8-fastbuild/bin/scala_targets/library.jar")
+      ScalacOptionsItem(
+        it,
+        emptyList(),
+        listOf(
+          "file://\$BAZEL_OUTPUT_BASE_PATH/execroot/__main__/bazel-out/k8-fastbuild/bin/external/io_bazel_rules_scala_scala_library/io_bazel_rules_scala_scala_library.stamp/scala-library-2.13.6-stamped.jar",
+          "file://\$BAZEL_OUTPUT_BASE_PATH/execroot/__main__/bazel-out/k8-fastbuild/bin/external/io_bazel_rules_scala_scala_reflect/io_bazel_rules_scala_scala_reflect.stamp/scala-reflect-2.13.6-stamped.jar"
+        ),
+        "file://\$BAZEL_OUTPUT_BASE_PATH/execroot/__main__/bazel-out/k8-fastbuild/bin/scala_targets/library.jar"
+      )
     }
     val expectedScalaOptionsResult = ScalacOptionsResult(expectedScalaOptionsItems)
     val scalaOptionsParams = ScalacOptionsParams(expectedTargetIdentifiers)
