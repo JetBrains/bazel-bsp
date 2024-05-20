a30a9ad89333f2efdf0d3d60550e00e7bac8c4b5
Michael Noah <mnoah1@uber.com>
2024-05-20 13:34:24 -0400

    Pass test filter to command

--- protocol/src/main/kotlin/org/jetbrains/bsp/BazelTestParamsData.kt
+++ protocol/src/main/kotlin/org/jetbrains/bsp/BazelTestParamsData.kt
@@ -1,7 +1,8 @@
 package org.jetbrains.bsp
 
 public data class BazelTestParamsData(
-        val coverage: Boolean
+        val coverage: Boolean?,
+        val testFilter: String?
 ) {
     companion object {
         const val DATA_KIND = "bazel-test"
